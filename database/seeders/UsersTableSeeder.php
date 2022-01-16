e24a76c3c15c377cdfe1895b70ce1b8bdc478100
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-16 23:36:15 +0800

    Create UsersTableSeeder.php

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'user_id' => 'UID001',
+            'hun_id' => 'HUN001',
+            'name' => 'Nurzarinah',
+            'email' => 'nrz.work@gmail.com',
+            'password' => \Hash::make('password'),
+            'phone_no' => '+60123456789',
+            'ic_no' => '1234567890',
+            'role' => 'superadmin'
+        ]);
+    }
+}
