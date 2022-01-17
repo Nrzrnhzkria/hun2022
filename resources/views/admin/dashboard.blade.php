5cf3a14d1f1a513af2af3517522ff5b70b5af13c
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 13:26:52 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+</div>
 
 <div class="container">
     <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
