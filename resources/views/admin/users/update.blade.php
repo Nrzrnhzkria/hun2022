caf981fb9f0a77b49a415c012ee8f1e264d6ff52
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 16:17:48 +0800

    Update update.blade.php

--- resources/views/admin/users/update.blade.php
+++ resources/views/admin/users/update.blade.php
@@ -9,10 +9,78 @@
 <div class="container">
     
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Update User</h1>
+        <h1 class="h2">User Information</h1>
     </div>
 
-    
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <form class="row g-3 px-3" method="POST" action="{{ url('updateuser') }}/{{ $users->id }}">
+                        @csrf
+
+                        <div class='col-md-12'>
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $users->name }}" required autocomplete="name" autofocus>
+            
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+            
+                        <div class='col-md-12'>
+                            <div class="form-group">
+                                <label for="email" >E-Mail</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $users->email }}" required autocomplete="email">
+            
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                                
+                        <div class='col-md-12'>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ $users->password }}" required autocomplete="new-password">
+            
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+            
+                        <div class='col-md-12'>
+                            <div class="form-group">
+                                <label for="password-confirm">Confirm Password</label>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="{{ $users->password }}" required autocomplete="new-password">
+                            </div>
+                        </div>
+            
+                        <div class='col-md-12'>
+                            <label for="role">Change Role</label>
+                            <br>
+                        </div>
+            
+                        <div class='col-md-12 pt-3'>
+                            <button type="submit" class="btn btn-primary float-right">
+                                <i class="bi bi-save pr-2"></i>Save Changes
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
