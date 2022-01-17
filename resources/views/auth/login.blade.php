5effd1eba18b2b7432e0126a1a5ecb4f133e153c
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 21:43:11 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center py-5">
+        <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
