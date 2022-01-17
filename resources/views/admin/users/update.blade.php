d323ad817b53986844c188d94312dcb34514c91e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 22:45:18 +0800

    Update update.blade.php

--- resources/views/admin/users/update.blade.php
+++ resources/views/admin/users/update.blade.php
@@ -107,7 +107,7 @@
                         <div class="col-md-4">                            
                             <label for="name">Role</label>
                             <select class="form-select" aria-label="Default select example">
-                                <option class="text-capitalize" value="{{ $user->role }}" selected readonly>{{ $user->role }}</option>
+                                <option class="text-capitalize" value="{{ $user->role }}" selected>{{ $user->role }}</option>
                                 <option value="superadmin">Superadmin</option>
                                 <option value="admin">Admin</option>
                                 <option value="advisor">Advisor</option>
@@ -131,6 +131,25 @@
                             <button type="submit" class="btn btn-primary float-right">
                                 <i class="bi bi-save"></i> Save Changes
                             </button>
+                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $user->id }}"><i class="bi bi-trash"></i></button>
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal{{ $user->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    Are you sure you want to delete this user ?
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <a class="btn btn-danger" href="{{ url('delete-user') }}/{{ $user->id }}">Delete</a>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
                         </div>
 
                     </form>
