5151ca1de0a61882c3163b4a0c92ce73af9be178
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 14:13:47 +0800

    Update view.blade.php

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -16,9 +16,9 @@
         <div class="col-md-2">
             <div class="card" style="width: 18rem;">
                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
+                  <a class="text-decoration-none text-dark" href=""><li class="list-group-item">An item</li></a>
+                  <a class="text-decoration-none text-dark" href=""><li class="list-group-item">A second item</li></a>
+                  <a class="text-decoration-none text-dark" href=""><li class="list-group-item">A third item</li></a>
                 </ul>
             </div>
         </div>
