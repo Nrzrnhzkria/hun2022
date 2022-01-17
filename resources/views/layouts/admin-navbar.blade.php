fac9ae9f8b971327b6c53fbf5e6c28679930c819
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-17 12:58:30 +0800

    Update admin-navbar.blade.php

--- resources/views/layouts/admin-navbar.blade.php
+++ resources/views/layouts/admin-navbar.blade.php
@@ -11,7 +11,7 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto fw-bold">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="/">Dashboard</a>
+                    <a class="nav-link text-dark" href="/"><i class="bi bi-house-fill"></i></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-dark" href="/">Seminar</a>
