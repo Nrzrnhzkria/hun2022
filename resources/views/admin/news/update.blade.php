b095b7d999194a43396947d333747199312af3d8
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-18 23:58:02 +0800

    Update update.blade.php

--- resources/views/admin/news/update.blade.php
+++ resources/views/admin/news/update.blade.php
@@ -46,6 +46,25 @@
                             <button type="submit" class="btn btn-primary">
                                 Update News
                             </button>
+                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $news->id }}"><i class="bi bi-trash"></i></button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal{{ $news->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                        Are you sure you want to delete this news ?
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <a class="btn btn-danger" href="{{ url('delete-news') }}/{{ $news->id }}">Delete</a>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
                         </div>
                         
                     </form>
