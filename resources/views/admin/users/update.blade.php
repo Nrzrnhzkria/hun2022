922c537dcb5b7b7352604ff96139f9f261306329
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-18 13:53:56 +0800

    Update update.blade.php

--- resources/views/admin/users/update.blade.php
+++ resources/views/admin/users/update.blade.php
@@ -12,16 +12,17 @@
         <h1 class="h2">User Information</h1>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
+    <div class="row justify-content-center py-5">
+        <div class="col-md-8">
+            <div class="card py-5">
                 <div class="card-body">
                     <form class="row g-3 px-3" method="POST" action="{{ url('edit') }}/{{ $user->id }}">
                         @csrf
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label>HUN ID</label>
+                        <div class="row mb-3">
+                            <label for="hun_id" class="col-md-4 col-form-label text-md-end">HUN ID</label>
+
+                            <div class="col-md-6">
                                 <input id="hun_id" type="text" class="form-control @error('hun_id') is-invalid @enderror" name="hun_id" value="{{ $user->hun_id }}" required autocomplete="hun_id">
             
                                 @error('hun_id')
@@ -32,9 +33,10 @@
                             </div>
                         </div>
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label for="name">Name</label>
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
             
                                 @error('name')
@@ -44,10 +46,11 @@
                                 @enderror
                             </div>
                         </div>
-            
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label for="email" >Email</label>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
             
                                 @error('email')
@@ -57,10 +60,11 @@
                                 @enderror
                             </div>
                         </div>
-                                
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="password">Password</label>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ $user->password }}" required autocomplete="new-password">
             
                                 @error('password')
@@ -70,17 +74,19 @@
                                 @enderror
                             </div>
                         </div>
-            
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="password-confirm">Confirm Password</label>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="{{ $user->password }}" required autocomplete="new-password">
                             </div>
                         </div>
-            
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label for="name">Phone No.</label>
+
+                        <div class="row mb-3">
+                            <label for="phone_no" class="col-md-4 col-form-label text-md-end">Phone No</label>
+
+                            <div class="col-md-6">
                                 <input id="phone_no" type="text" class="form-control @error('phone_no') is-invalid @enderror" name="phone_no" value="{{ $user->phone_no }}" required autocomplete="phone_no">
             
                                 @error('phone_no')
@@ -91,11 +97,12 @@
                             </div>
                         </div>
 
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label for="name">IC No.</label>
+                        <div class="row mb-3">
+                            <label for="ic_no" class="col-md-4 col-form-label text-md-end">IC No</label>
+
+                            <div class="col-md-6">
                                 <input id="ic_no" type="text" class="form-control @error('ic_no') is-invalid @enderror" name="ic_no" value="{{ $user->ic_no }}" required autocomplete="ic_no">
-            
+
                                 @error('ic_no')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -104,7 +111,7 @@
                             </div>
                         </div>
 
-                        <div class="col-md-4">                            
+                        <div class="row mb-3">
                             <label for="name">Role</label>
                             <select class="form-select" aria-label="Default select example">
                                 <option class="text-capitalize" value="{{ $user->role }}" selected>{{ $user->role }}</option>
@@ -115,48 +122,40 @@
                                 <option value="vendor">Vendor</option>
                                 <option value="user">User</option>
                             </select>
-                            {{-- <div class="form-group">
-                                <label for="name">Role</label>
-                                <input id="role" type="text" class="form-control @error('role') is-invalid @enderror" name="role" value="{{ $user->role }}" required autocomplete="role">
-            
-                                @error('role')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div> --}}
                         </div>
-            
-                        <div class="col-md-12 pt-3">
-                            <button type="submit" class="btn btn-primary float-right">
-                                <i class="bi bi-save"></i> Save Changes
-                            </button>
-                            <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $user->id }}"><i class="bi bi-trash"></i></button>
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal{{ $user->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary float-right">
+                                    <i class="bi bi-save"></i> Save Changes
+                                </button>
+                                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $user->id }}"><i class="bi bi-trash"></i></button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal{{ $user->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                        Are you sure you want to delete this user ?
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <a class="btn btn-danger" href="{{ url('delete-user') }}/{{ $user->id }}">Delete</a>
+                                        </div>
                                     </div>
-                                    <div class="modal-body">
-                                    Are you sure you want to delete this user ?
                                     </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <a class="btn btn-danger" href="{{ url('delete-user') }}/{{ $user->id }}">Delete</a>
-                                    </div>
-                                </div>
                                 </div>
                             </div>
                         </div>
-
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
 </div>
 
 @endsection
