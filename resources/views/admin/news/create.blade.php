ee94c7757ecf9c061dd86e46858a840b598f6867
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-18 19:06:21 +0800

    Update create.blade.php

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,51 +8,67 @@
 
 <div class="container">
 
-    
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
         <h1>Create News</h1>
     </div>
 
-    <!-- Add package form ---------------------------------------------------->
-    <form class="row g-3 px-3" action="/emailtemplate/add" method="POST" id="dynamic_form" enctype="multipart/form-data"> 
-        @csrf
-        <div class='row my-3'>
-            <div class='col-md-6'>         
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input name="name" type="text" class="form-control" required>
-                </div>
-            </div>
-    
-            <div class='col-md-6'>
-                <div class="form-group">
-                    <label for="name">Title</label>
-                    <input name="title" type="text" class="form-control" required>
-                </div>
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class='col-md-6'>
-                <div class="form-group">
-                    <label for="name">Content</label>
-                    <textarea name="content" class="ckeditor form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-            </div>
+    <div class="row justify-content-center py-5">
+        <div class="col-md-8">
+            <div class="card py-5">
+                <div class="card-body">
+                    
+                    <form action="/emailtemplate/add" method="POST" id="dynamic_form" enctype="multipart/form-data"> 
+                        @csrf
+                        <div class="row mb-3">
+                            <div class="col-md-6">         
+                                <div class="form-group">
+                                    <label for="title">Title</label>
+                                    <input name="title" type="text" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="teaser">Teaser</label>
+                                    <textarea type="text" class="form-control" placeholder="This teaser will be shown in landing page"  name="teaser"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="content">Content</label>
+                                    <textarea name="content" class="ckeditor form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+                        </div>
 
-            <div class='col-md-6'>
-                <div class="form-group">
-                    <label for="name">Date</label>
-                    <input name="date" type="date" class="form-control" required>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="img_name">Upload Image</label>
+                                    <input class="form-control" name="img_name" type="file" id="formFile">
+                                </div>
+                            </div>
+                        </div>
+                            
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create News
+                                </button>
+                            </div>
+                        </div>
+                        
+                    </form>
+
                 </div>
             </div>
         </div>
-            
-        <div class='col-md-8 pt-3'>
-            <button type='submit' class='btn btn-primary'> <i class="fas fa-save pr-1"></i> Save</button>
-        </div>
-        
-    </form>
+    </div>
  
 </div>
 
