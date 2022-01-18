a3bb158eb33f171844bc6567ffa8f3dc761e5c14
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-18 23:16:06 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -200,7 +200,7 @@
 {{-- Countdown --}}
 <script>
     // Set the date we're counting down to
-    var countDownDate = new Date("Feb 2, 2022 15:37:25").getTime();
+    var countDownDate = new Date("Mar 24, 2022 15:37:25").getTime();
     
     // Update the count down every 1 second
     var x = setInterval(function() {
