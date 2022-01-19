1b97a1ed0bd6d8d47c3966adb5c35e01bf19a302
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 17:24:24 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -179,24 +179,24 @@
 
     <div class="row">
         <div class="brand-carousel owl-carousel">
-            <div class="single-logo">
+            {{-- <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/QxPJ8hXy/brand-1.png" alt="">
-            </div>
-            <div class="single-logo">
+            {{-- </div>
+            <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/pdMQjC5Q/brand-2.png" alt="">
-            </div>
-            <div class="single-logo">
+            {{-- </div>
+            <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/B6qxYvgX/brand-3.png" alt="">
-            </div>
-            <div class="single-logo">
+            {{-- </div>
+            <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/d14GzKHn/brand-4.png" alt="">
-                </div>
-            <div class="single-logo">
+                {{-- </div>
+            <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/x8ZM13Sz/brand-5.png" alt="">
-            </div>
-            <div class="single-logo">
+            {{-- </div>
+            <div class="single-logo"> --}}
                 <img src="https://i.postimg.cc/B6qxYvgX/brand-3.png" alt="">
-            </div>
+            {{-- </div> --}}
         </div>
     </div>
     
