0746cc933e2047700839570c2ceea4a65a068a59
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 16:16:34 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -19,7 +19,7 @@
     .timer div {
         padding: 30px;
         border-radius: 3px;
-        background: #000000;
+        background: orange;
         display: inline-block;
         font-size: 40px;
         font-weight: 400;
