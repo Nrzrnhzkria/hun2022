f5dda2cba297a6b936ba6eb943564720d74dff5d
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 22:58:32 +0800

    Update news.blade.php

--- resources/views/landingpage/news.blade.php
+++ resources/views/landingpage/news.blade.php
@@ -7,23 +7,27 @@
 @section('content')
 
 <div class="container py-4">
-    @foreach ($news as $new)
-    <a href="/news" class="text-dark text-decoration-none">
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $new->title }}</h5>
-                        <p class="card-text">{{ $new->teaser}}</p>
+    <div class="row">
+        <div class="col-md-12">
+            @foreach ($news as $new)
+            <a href="/news" class="text-dark text-decoration-none">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $new->title }}</h5>
+                                <p class="card-text">{{ $new->teaser}}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
+            @endforeach
         </div>
-    </a>
-    @endforeach
+    </div>
 </div>
 
 @endsection
