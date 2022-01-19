77c463d9d04505a96f3cd93035354b88eeec2b3b
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 23:04:24 +0800

    update

--- resources/views/landingpage/news.blade.php
+++ resources/views/landingpage/news.blade.php
@@ -8,25 +8,24 @@
 
 <div class="container py-4">
     <div class="row px-5">
-        <div class="col-md-12">
-            @foreach ($news as $new)
-            <a href="/news" class="text-dark text-decoration-none">
-                <div class="card mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $new->title }}</h5>
-                                <p class="card-text">{{ $new->teaser}}</p>
-                            </div>
+        <h1 class="border-bottom">News</h1>
+        @foreach ($news as $new)
+        <a href="/news" class="text-dark text-decoration-none">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $new->title }}</h5>
+                            <p class="card-text">{{ $new->teaser}}</p>
                         </div>
                     </div>
                 </div>
-            </a>
-            @endforeach
-        </div>
+            </div>
+        </a>
+        @endforeach
     </div>
 </div>
 
--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="container py-5">
-    <div class="row">
+    <div class="row px-5">
         <h1 class="border-bottom">New Registration Form</h1>
             {{-- <form action="{{ url('save') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->stud_id }}" method="POST">
                 @csrf --}}
