d7611d12371334f6750d0068560593141c91ba2c
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 23:07:32 +0800

    Update news.blade.php

--- resources/views/landingpage/news.blade.php
+++ resources/views/landingpage/news.blade.php
@@ -8,7 +8,9 @@
 
 <div class="container py-4">
     <div class="row px-5">
-        <h1 class="border-bottom pb-3">News</h1>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
+            <h1>News</h1>
+        </div>
         @foreach ($news as $new)
         <a href="/news" class="text-dark text-decoration-none">
             <div class="card mb-3">
