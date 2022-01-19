f6d942121c1c05013527621bd8b9ce88ca48972d
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 20:48:33 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -117,7 +117,7 @@
                 @foreach ($news->take(4) as $new)
                     <td>
                         <div class="col-md-3">
-                            <div class="card" style="width: 18rem; height: 25rem;">
+                            <div class="card" style="width: 18rem; height: 30rem;">
                                 <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                 <h5 class="card-title">{{ $new->title }}</h5>
