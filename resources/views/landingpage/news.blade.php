19bad9b99a6f8fd11f831e3cd14c854d9da1be85
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 22:57:16 +0800

    Update news.blade.php

--- resources/views/landingpage/news.blade.php
+++ resources/views/landingpage/news.blade.php
@@ -8,21 +8,21 @@
 
 <div class="container py-4">
     @foreach ($news as $new)
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <a href="/news" class="text-dark text-decoration-none">
+    <a href="/news" class="text-dark text-decoration-none">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">{{ $new->title }}</h5>
                         <p class="card-text">{{ $new->teaser}}</p>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
-    </div>
+    </a>
     @endforeach
 </div>
 
