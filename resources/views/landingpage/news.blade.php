a53cf0f6ba67888b43848ec7d20b57200ea44bb5
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 23:20:28 +0800

    Update news.blade.php

--- resources/views/landingpage/news.blade.php
+++ resources/views/landingpage/news.blade.php
@@ -12,16 +12,18 @@
             <h1>News</h1>
         </div>
         @foreach ($news as $new)
-        <div class="card mb-3">
+        <div class="col-md-12">
             <a href="/news" class="text-dark text-decoration-none">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $new->title }}</h5>
-                            <p class="card-text">{{ $new->teaser}}</p>
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="{{ $new->img_name }}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $new->title }}</h5>
+                                <p class="card-text">{{ $new->teaser}}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
