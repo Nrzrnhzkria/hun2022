fb7bccdf060240f1288c0b41f8b08fe38140ebf6
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 22:11:49 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -117,16 +117,16 @@
                 @foreach ($news->take(4) as $new)
                     <td>
                         <div class="col-md-3">
-                            <a href="/news" class="text-dark text-decoration-none">
-                                <div class="card" style="width: 16.5rem; height: 23rem;">
-                                    <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                    <h5 class="card-title">{{ $new->title }}</h5>
-                                    {{-- <p class="card-text">{{ $new->teaser}}</p> --}}
-                                    {{-- <a href="#" class="btn btn-warning">See More >></a> --}}
-                                    </div>
+                            {{-- <a href="/news" class="text-dark text-decoration-none"> --}}
+                            <div class="card" style="width: 16.5rem; height: 24rem;">
+                                <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                <h5 class="card-title">{{ $new->title }}</h5>
+                                {{-- <p class="card-text">{{ $new->teaser}}</p> --}}
+                                <a href="/news" class="btn btn-warning">See More >></a>
                                 </div>
-                            </a>
+                            </div>
+                            {{-- </a> --}}
                         </div> 
                     </td>        
                 @endforeach
