e053765fcc955a9e05d3b287fedba52b959fa9b5
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 23:33:45 +0800

    Update readmore.blade.php

---
+++ resources/views/landingpage/news/readmore.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+    News
+@endsection
+
+@section('content')
+
+<div class="container">
+    <div class="row px-5">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mb-3 border-bottom">
+            <h1>News</h1>
+        </div>
+        <div class="col-md-12">
+            
+        </div>
+    </div>
+</div>
+
+@endsection
