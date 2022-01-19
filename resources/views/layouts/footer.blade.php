0d935a046e062aab3b1d3fc4d96e8d96ae1dbb25
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 16:50:29 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,68 +1,66 @@
-<div class="row">
-    <div class="container">
-        <footer class="row row-cols-6 py-5 my-5 border-top">
-            <div class="col">
-                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-                    <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="50%">
-                </a>
-            </div>
-      
-            <div class="col">
-                <h5>SECRETARIAT</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">No 42B, Jalan Eko Botani 3/2, Taman Eko Botani, 79100 Iskandar Puteri, Johor</p></li>
-                    <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">Tel: +60 7-509 9897</p></li>
-                    <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">Email: <br> jivan@shekhinah.my <br> chris@shekhinah.my </a></li>
-                </ul>
-            </div>
-      
-            <div class="col">
-                <h5>NAVIGATION</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
-                    <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
-                    <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
-                    <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
-                </ul>
-            </div>
-        
-            <div class="col">
-                <h5>NEWSROOM</h5>
-                <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Malaysian Sports</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">International Sports</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">News</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Events</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Reports</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Reviews</a></li>
-                </ul>
-            </div>
-        
-            <div class="col">
-                <h5>LEGAL</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
-                </ul>
-            </div>
+<div class="container">
+    <footer class="row-fluid row-cols-6 py-5 my-5 border-top">
+        <div class="col">
+            <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="50%">
+            </a>
+        </div>
+  
+        <div class="col">
+            <h5>SECRETARIAT</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">No 42B, Jalan Eko Botani 3/2, Taman Eko Botani, 79100 Iskandar Puteri, Johor</p></li>
+                <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">Tel: +60 7-509 9897</p></li>
+                <li class="nav-item mb-2"><p class="nav-link p-0 text-muted">Email: <br> jivan@shekhinah.my <br> chris@shekhinah.my </a></li>
+            </ul>
+        </div>
+  
+        <div class="col">
+            <h5>NAVIGATION</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
+                <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
+                <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
+                <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
+                <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
+            </ul>
+        </div>
     
-            <div class="col">
-                <h5>SOCIAL MEDIA</h5>
-                <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
-                </ul>
-            </div>
+        <div class="col">
+            <h5>NEWSROOM</h5>
+            <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Malaysian Sports</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">International Sports</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">News</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Events</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Reports</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Reviews</a></li>
+            </ul>
+        </div>
     
-        </footer>
-    
-        <p class="text-center pb-4">All rights reserved &copy; 2022 Hari Usahawan Negara</p>
-    
-    </div>
+        <div class="col">
+            <h5>LEGAL</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
+            </ul>
+        </div>
+
+        <div class="col">
+            <h5>SOCIAL MEDIA</h5>
+            <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
+            </ul>
+        </div>
+
+    </footer>
+
+    <p class="text-center pb-4">All rights reserved &copy; 2022 Hari Usahawan Negara</p>
+
 </div>
