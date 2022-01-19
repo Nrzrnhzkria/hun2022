2995b01deae894414e2a031e589fce054311fbf4
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 23:44:19 +0800

    Update readmore.blade.php

--- resources/views/landingpage/news/readmore.blade.php
+++ resources/views/landingpage/news/readmore.blade.php
@@ -9,7 +9,7 @@
 <div class="container">
     <div class="row px-5">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mb-3 border-bottom">
-            <h1>News</h1>
+            <h1>{{ $news->title }}</h1>
         </div>
         <div class="col-md-12">
             <div class="card px-4 py-4">
@@ -18,8 +18,6 @@
                         <img class="img-fluid" src="{{ $news->img_name }}" alt="">
                     </div>
                 </div>
-
-                <div class="fw-bold px-2 py-2" style="background-color: orange">{{ $news->title }}</div>
 
                 <p class="text-justify">{{ $news->content }}</p>
             </div>
