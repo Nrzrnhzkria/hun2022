9387a79813b598d22403302410df62d1af346f07
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-19 20:57:02 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -117,7 +117,7 @@
                 @foreach ($news->take(4) as $new)
                     <td>
                         <div class="col-md-3">
-                            <a href="/news">
+                            <a href="/news" class="text-decoration-none">
                                 <div class="card" style="width: 16.5rem; height: 23rem;">
                                     <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
                                     <div class="card-body">
