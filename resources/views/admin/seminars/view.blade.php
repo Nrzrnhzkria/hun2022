6a7f5e312e620a47a29696be1d49a03d500d830e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-20 11:19:34 +0800

    Update view.blade.php

--- resources/views/admin/seminars/view.blade.php
+++ resources/views/admin/seminars/view.blade.php
@@ -9,7 +9,7 @@
 <div class="container">
     
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Seminar</h1>
+        <h1 class="h2">Seminar Attendance</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <a href="/create-seminar" class="btn btn-outline-dark"><i class="bi bi-plus-lg"></i> Add Seminar</a>
@@ -65,7 +65,7 @@
                 </table>
             </div>
             @else
-              <p>There are no seminar to display.</p>
+              <p>There are no attendee to display.</p>
             @endif
         </div>
     </div>
