a23e3b57913a079078fabecfb69178be3a0f94ba
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-20 10:44:56 +0800

    Update view.blade.php

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -38,9 +38,7 @@
     <div class="row pb-3">
         
         <div class="col-md-9">
-            <div class="d-flex justify-content-center">
-                {!! $users->render() !!}
-            </div>
+            <div  class="pagination float-right pt-3">{{ $users->links() }}</div>
             @if(count($users) > 0)
             <div class="table-responsive">
                 <table class="table">
