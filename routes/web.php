7ee9c50512743b2b0f124c7c6373ae42d11c8667
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 00:22:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::post('store-qr', 'SeminarQRController@store_qr');
 Route::get('update-qr/{qr_id}', 'SeminarQRController@update_qr');
 Route::post('edit-qr/{qr_id}', 'SeminarQRController@edit_qr');
 Route::get('delete-qr/{qr_id}', 'SeminarQRController@destroy_qr');
-Route::post('register-seminar/{qr_id}', 'SeminarQRController@registeruser');
+Route::get('register-seminar/{qr_id}', 'SeminarQRController@registeruser');
 Route::post('download-qr/{type}/{qr_id}', 'SeminarQRController@downloadQRCode')->name('qrcode.download');
 Route::get('register-success/{qr_id}/{user_id}', 'SeminarQRController@registersuccess');
 // Vendor Management
