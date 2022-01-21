f15ee4d71d2b23f58f7bf4223df7f62c8c100b17
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 00:43:47 +0800

    Update preface.blade.php

--- resources/views/landingpage/about/preface.blade.php
+++ resources/views/landingpage/about/preface.blade.php
@@ -15,12 +15,24 @@
                 </div>
             </div>
 
-            <div class="text-center fw-bold px-2 py-2" style="background-color: orange">PREAMBLE BY YBHG DATO' SERI ABU HASAN BIN MOHD NOR</div>
+            <div class="text-center fw-bold px-2 py-2" style="background-color: orange">OPENING REMARKS BY YBHG DATO' SERI ABU HASAN BIN MOHD NOR</div>
 
-            <p class="text-justify">Thank god I prayed to hadrat llāhi with the meal and kindness still again
-                given the opportunity and the strength to lead the Board of Commerce Small Entrepreneur Malay-
-                sia (DPUKM), may be strengthened spirit to continue to strengthen the friendship between the business-
-                wan-entrepreneurs in DPUKM in particular and the whole of Malaysia.</p>
+            <p class="text-justify">&nbsp;&nbsp;&nbsp;Alhamdulillah, All praises are to Allah, the Merciful, the All Beneficient, by
+                Grace and Blessings who has favoured us and of His believing servants and still giving me an 
+                opportunity and strength to lead the DEWAN PERNIAGAAN USAHAWAN KECIL MALAYSIA (DPUKM), and for those 
+                desiring entrepreneurs a long way in winning over their trust and building a strong connection between 
+                each other through DPUKM especially in community and Malaysia. Despite, Congratulation and deepest 
+                gratitude to Members of Highest Council for the commitment and contribution in realizing the vision and 
+                mission DPUKM in inventing a best platform as an opportunities for ambitious start-up, prosperous 
+                entrepreneur and venture professionals in Malaysia. Whilst juggling with career and personal life, as 
+                entrepreneurs focus on relational value such as building network and expanding circle of acquaintances 
+                should be nurture and keep in view/adopted. To ensure equitable economic development to the entrepreneurs 
+                and produce balance business and industry ecosystem to strengthen and enhance cooperation of entrepreneurs, 
+                numerous activities and  workforce are arranged by me and Members of Highest Council. As human being, our 
+                life is mapped out on the plans and decisions of Allah SWT and  as human being we can comprehend it only as 
+                far as Allah permits to do so . Despite, we should not discourage and strive to achieve what we aspire to, 
+                while recognizing that Allah has the perfect plan.
+                </p>
             
         </div>
     </div>
