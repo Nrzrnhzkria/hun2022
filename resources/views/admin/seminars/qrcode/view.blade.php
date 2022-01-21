57fa98515e0842e5acf1c792824ac14bfd8c6b43
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 00:20:43 +0800

    Update view.blade.php

--- resources/views/admin/seminars/qrcode/view.blade.php
+++ resources/views/admin/seminars/qrcode/view.blade.php
@@ -59,7 +59,7 @@
                             <td>{{ $seminarqr->seminar_date }}</td>
                             <td>
                                 {{-- {{ route('qrcode.download', [ 'type' => 'png' ])}} <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->errorCorrection('H')->size(50)->generate('codingdriver.com')) !!}" /> --}}
-                                <form class="form-horizontal" action="{{ url('download-qr/png') }}/{{ $qr->id }}" method="post">
+                                <form class="form-horizontal" action="{{ url('download-qr/png') }}/{{ $seminarqr->id }}" method="post">
                                     @csrf
                                     <button type="submit" class="align-middle btn btn-outline-dark">
                                         <i class="bi bi-download"></i> Download PNG
