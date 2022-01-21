02e0f4ae045824fcc33b99cdaf481b9b7aa4d403
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-21 19:45:41 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -57,17 +57,18 @@
       </div>
       <div class="col-md-4">
         <div class="card border-0 shadow text-center" style="height: 22rem">
-          <canvas id="memberChart" style="width:100%; max-width:600px; height:80%; max-height:800px; padding-top: 1.5rem"></canvas>
-          <div class="table-responsive px-3">
+          <h6 class="pt-4">HUN Membership</h6>
+          <canvas id="memberChart" style="width:100%; max-width:600px; height:80%; max-height:800px;"></canvas>
+          <div class="table-responsive p-3">
             <table class="table table-bordered table-sm">
               <tr>
                 <td class="text-end w-50">Members</td>
-                <td>:</td>
+                <td style="width: 1rem">:</td>
                 <td class="text-start">{{ $member }}</td>
               </tr>
               <tr>
                 <td class="text-end w-50">Non-members</td>
-                <td>:</td>
+                <td style="width: 1rem">:</td>
                 <td class="text-start">{{ $nonmember }}</td>
               </tr>
             </table>
@@ -251,7 +252,7 @@
     },
     options: {
       title: {
-        display: true,
+        display: false,
         text: "HUN Membership"
       }
     }
