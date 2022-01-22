665f432332d201c63ac62eee1f09e61fa50747c8
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-23 03:43:20 +0800

    Update success.blade.php

--- resources/views/landingpage/register/success.blade.php
+++ resources/views/landingpage/register/success.blade.php
@@ -9,8 +9,8 @@
     <div class="row p-5">
         <div class="text-center">
         <h3>You have been registered successfully!</h3>
-        <div class="py-3" style="font-size: 24rem; color: green;">
-            <i class="bi bi-check-circle-fill"></i>
+        <div class="py-3" style="font-size: 10rem; color: green;">
+            <i class="bi bi-check2-circle"></i>
         </div>
         <hr>
         <p> The receipt will be send to your registered email address in 24 hours. Thank you for your patient.</p>
