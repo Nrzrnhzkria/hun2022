cb9dbcb798114a7b482eda94926f79327f970dd8
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 10:18:24 +0800

    Update SeminarQRController.php

--- app/Http/Controllers/SeminarQRController.php
+++ app/Http/Controllers/SeminarQRController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\SeminarQR;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use App\User;
+use App\Models\User;
 use Auth;
 
 class SeminarQRController extends Controller
