92abfd905002a72b4ea43da39fb485ba03342820
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 12:25:01 +0800

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -33,7 +33,7 @@ class TransactionController extends Controller
 
         $url = 'https://dev.toyyibpay.com/index.php/api/createBill';
         $response = Http::asForm()->post($url, $data);
-        $bill_code = $response[0]['BillCode'];
+        // $bill_code = $response[0]['BillCode'];
 
         dd($response);
 
