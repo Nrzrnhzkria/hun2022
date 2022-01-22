96788389e009bd5453bf7fd094daa18362cbd54c
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 10:25:32 +0800

    Update SeminarQRController.php

--- app/Http/Controllers/SeminarQRController.php
+++ app/Http/Controllers/SeminarQRController.php
@@ -99,8 +99,8 @@ class SeminarQRController extends Controller
     public function registeruser($qr_id){
 		$user_id = Auth::user()->id;
         $get_user = User::where('id', $user_id)->first();
-        dd($get_user);
-        // $register = SeminarAttendance::where('seminar_id', $qr_id)->first();
+        $register = SeminarAttendance::where('seminar_id', $qr_id)->first();
+        dd($register);
 
         // $register->user_id = $user_id;
         // $register->seminar_id = $qr_id;
