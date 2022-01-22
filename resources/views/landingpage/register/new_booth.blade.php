599c42c482d992c6b223c5920e9a75783ace2e28
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-23 02:17:17 +0800

    Update new_booth.blade.php

--- resources/views/landingpage/register/new_booth.blade.php
+++ resources/views/landingpage/register/new_booth.blade.php
@@ -16,7 +16,7 @@
                 <div class="card px-4 py-4">
                     <ul class="nav nav-tabs px-2 py-2">
                         <li class="nav-item">
-                          <a class="nav-link" href="{{ url('new-registration') }}/{{ $vendor->ic_no }}">Step 1</a>
+                          <a class="nav-link disabled">Step 1</a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link active" aria-current="page">Step 2</a>
@@ -30,13 +30,193 @@
                     </ul>
 
                     <div class="fw-bold px-2 py-2" style="background-color: orange">Choose Your Booth Type</div>
-    
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul class="px-3">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
                         <div class="row p-3">
 
                             @if($details->nationality == 'local')
-                                <div class="col-md-6 pb-2">
-                                    <label for="formFileMultiple" class="form-label">Local:</label>
-                                    <input class="form-control" type="file" name="img_name" id="formFileMultiple" multiple>
+                                <div class="col-md-12 pb-2">
+                                    <div class="table-responsive">
+                                        <table class="table table-sm">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Hall Booth</th>
+                                                  <th scope="col">Lot Placement</th>
+                                                  <th scope="col">Price (RM)</th>
+                                                  <th scope="col">Please Choose One</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">1</th>
+                                                    <td>PREMIUM</td>
+                                                    <td>Island</td>
+                                                    <td>500,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="500000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">2</th>
+                                                    <td>36M x 24M</td>
+                                                    <td>Island</td>
+                                                    <td>250,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="250000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">3</th>
+                                                    <td>24M x 12M</td>
+                                                    <td>Island</td>
+                                                    <td>180,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="180000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">4</th>
+                                                    <td>24M x 24M</td>
+                                                    <td>Island</td>
+                                                    <td>100,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="100000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">5</th>
+                                                    <td>12M x 12M</td>
+                                                    <td>Island</td>
+                                                    <td>50,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="50000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">6</th>
+                                                    <td>6M x 12M</td>
+                                                    <td>Island</td>
+                                                    <td>25,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="25000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">7</th>
+                                                    <td>6M x 6M</td>
+                                                    <td>Island</td>
+                                                    <td>6,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="6000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">8</th>
+                                                    <td>6M x 3M</td>
+                                                    <td>Terrace</td>
+                                                    <td>5,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="5000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">9</th>
+                                                    <td>3M x 3M</td>
+                                                    <td>Terrace</td>
+                                                    <td>3,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="3000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row"></th>
+                                                    <td>FOYER BOOTH</td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">10</th>
+                                                    <td>6M x 6M</td>
+                                                    <td>Island</td>
+                                                    <td>6,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="6000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">11</th>
+                                                    <td>3M x 3M</td>
+                                                    <td>Terrace</td>
+                                                    <td>3,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="3000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row"></th>
+                                                    <td>FOOD COURT</td>
+                                                    <td>RIVERSIDE</td>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">12</th>
+                                                    <td>6M x 3M</td>
+                                                    <td></td>
+                                                    <td>1,500</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="6000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">13</th>
+                                                    <td>6M x 6M</td>
+                                                    <td></td>
+                                                    <td>3,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="3000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">14</th>
+                                                    <td>Food-Truck Space</td>
+                                                    <td></td>
+                                                    <td>1,000</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="1000">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">15</th>
+                                                    <td>Car-Boot Park Spot</td>
+                                                    <td></td>
+                                                    <td>250 (5 days)</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="250">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">16</th>
+                                                    <td>Food Ride</td>
+                                                    <td></td>
+                                                    <td>250 (5 days)</td>
+                                                    <td>
+                                                        <input type="radio" name="amount" value="250">
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             @elseif($details->nationality == 'international')
                                 <div class="col-md-6 pb-2">
