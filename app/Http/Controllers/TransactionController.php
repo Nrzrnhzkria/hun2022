be886a73fc0ded40e59613276b938330d90e449a
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-22 12:24:10 +0800

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -35,7 +35,9 @@ class TransactionController extends Controller
         $response = Http::asForm()->post($url, $data);
         $bill_code = $response[0]['BillCode'];
 
-        return redirect('https://dev.toyyibpay.com/' . $bill_code);
+        dd($response);
+
+        // return redirect('https://dev.toyyibpay.com/' . $bill_code);
     }
 
     public function payment_status(){
