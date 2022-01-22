7cb9ce03652a7689edbc0d51931298f32d90d77d
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-23 04:23:06 +0800

    Update VendorController.php

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -166,11 +166,11 @@ class VendorController extends Controller
 
         $url = 'https://toyyibpay.com/index.php/api/createBill';
         $response = Http::asForm()->post($url, $data);
-        // $bill_code = $response->json()[0]['BillCode'];
+        $bill_code = $response->json()[0]['BillCode'];
 
-        dd($amount);
+        // dd($amount);
         // dd($response->json()); // to know error
-        // return redirect('https://toyyibpay.com/' . $bill_code);
+        return redirect('https://toyyibpay.com/' . $bill_code);
     }
 
     public function payment_status(Request $request){
