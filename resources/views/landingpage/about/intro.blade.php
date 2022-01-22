4ca538b6c05c2603add744ba24d63b1c5b7088a5
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-23 04:39:41 +0800

    Update intro.blade.php

--- resources/views/landingpage/about/intro.blade.php
+++ resources/views/landingpage/about/intro.blade.php
@@ -8,7 +8,7 @@
 
 <div class="container py-4">
     <div class="row">
-        <div class="card px-4 py-4">
+        <div class="card px-4 py-4" style="background-image: url('{{ asset('assets/img/about/introbg.png') }}');">
             <div class="row pb-4">
                 <div class="col-md-12 text-center">        
                     <img class="img-fluid" src="{{ asset('assets/img/about/dpukm.png') }}" alt="" style="width: 20rem">
@@ -17,7 +17,7 @@
 
             <div class="text-center fw-bold px-2 py-2" style="background-color: orange">INTRODUCTION</div>
 
-            <div class="row p-4" style="background-image: url('{{ asset('assets/img/about/introbg.png') }}'); width:100%;">
+            <div class="row p-4">
                 <p style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DEWAN PERNIAGAAN USAHAWAN KECIL MALAYSIA,
                     abbreviated  (DPUKM) was formed on Mac 2013 is a platform for entrepreneurs that handle by 100% Bumiputera 
                     in collaboration with Ministries, Departments, Agencies, Cooperatives and the Private Sector. 
