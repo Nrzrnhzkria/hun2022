38c95459f0dd6fef2ba762abe661dab61be1ede6
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-24 15:01:00 +0800

    Update contact.blade.php

--- resources/views/landingpage/contact.blade.php
+++ resources/views/landingpage/contact.blade.php
@@ -7,8 +7,30 @@
 @section('content')
 
 <div class="container py-4">
-    <div class="mapouter"><div class="gmap_canvas"><iframe width="639" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=3.158426207303803,%20101.70054294420589&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123moviesz.nl"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://googlemapsembedcodegenerator.com
-        "> how to add a map to wordpress </a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:639px;}</style></div></div>
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=no%2030-2,%20jalan%209/23A,%20off%20jalan%20usahawan,%2053200%20setapak&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <a href="https://fmovies-online.net"></a>
+            <br>
+            <style>
+                .mapouter{
+                    position:relative;
+                    text-align:right;
+                    height:500px;
+                    width:600px;
+                }
+            </style>
+            <a href="https://www.embedgooglemap.net">embed custom google map</a>
+            <style>
+                .gmap_canvas {
+                    overflow:hidden;
+                    background:none!important;
+                    height:500px;
+                    width:600px;
+                }
+            </style>
+        </div>
+    </div>
 </div>
 
 @endsection
