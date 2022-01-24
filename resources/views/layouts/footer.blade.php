4df37ab5324c872cf80f4796666218677dce408c
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-24 15:39:41 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,41 +1,43 @@
 <div class="container">
     <footer class="row py-5 my-5 border-top">
-        <div class="col-md-1 pb-3">
-            <a href="/" class="d-flex align-items-center link-dark text-decoration-none">
-                <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="60rem">
-            </a>
-        </div>
+        <div class="container">
+            <div class="col-md-1 pb-3">
+                <a href="/" class="d-flex align-items-center link-dark text-decoration-none">
+                    <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="60rem">
+                </a>
+            </div>
 
-        <div class="col-md-4 text-center pb-3">
-            <h5>NAVIGATION</h5>
-            <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
-                <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
-                <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
-                <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
-                <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
-            </ul>
-        </div>
+            <div class="col-md-4 text-center pb-3">
+                <h5>NAVIGATION</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
+                    <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
+                    <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
+                    <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
+                    <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
+                </ul>
+            </div>
 
-        <div class="col-md-4 text-center pb-3">
-            <h5>LEGAL</h5>
-            <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
-            </ul>
-        </div>
-        
-        <div class="col-md-3 text-center pb-3">
-            <h5>SOCIAL MEDIA</h5>
-            <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
-            </ul>
+            <div class="col-md-4 text-center pb-3">
+                <h5>LEGAL</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
+                </ul>
+            </div>
+            
+            <div class="col-md-3 text-center pb-3">
+                <h5>SOCIAL MEDIA</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
+                </ul>
+            </div>
         </div>
 
     </footer>
