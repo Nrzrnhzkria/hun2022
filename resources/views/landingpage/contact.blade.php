003859d573476aabaa9571673dd222117f2afecf
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-24 15:29:08 +0800

    Update contact.blade.php

--- resources/views/landingpage/contact.blade.php
+++ resources/views/landingpage/contact.blade.php
@@ -26,15 +26,15 @@
 
             <div class="mapouter">
                 <div class="gmap_canvas">
-                    <iframe width="500rem" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=no%2030-2,%20jalan%209/23A,%20off%20jalan%20usahawan,%2053200%20setapak&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    <iframe width="600rem" height="500rem" id="gmap_canvas" src="https://maps.google.com/maps?q=no%2030-2,%20jalan%209/23A,%20off%20jalan%20usahawan,%2053200%20setapak&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
                     <a href="https://fmovies-online.net"></a>
                     <br>
                     <style>
                         .mapouter{
                             position:relative;
-                            text-align:right;
-                            height:500px;
-                            width:600px;
+                            text-align:left;
+                            height:500rem;
+                            width:600rem;
                         }
                     </style>
                     <a href="https://www.embedgooglemap.net">embed custom google map</a>
@@ -42,8 +42,8 @@
                         .gmap_canvas {
                             overflow:hidden;
                             background:none!important;
-                            height:500px;
-                            width:600px;
+                            height:500rem;
+                            width:600rem;
                         }
                     </style>
                 </div>
