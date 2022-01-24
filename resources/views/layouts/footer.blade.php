4b100437b8c204b9e5f6106040d44dffb80cfb50
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-24 16:02:04 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,43 +1,46 @@
 <div class="container">
     <footer class="row py-5 my-5 border-top">
-        <table class="table table-bordered text-center" style="width: 500rem">
-            <tr>
-                <td>
-                    <a href="/" class="d-flex align-items-center link-dark text-decoration-none">
-                        <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="60rem">
-                    </a>
-                </td>
-                <td>
-                    <h5>NAVIGATION</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
-                        <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
-                        <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
-                        <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
-                        <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
-                    </ul>
-                </td>
-                <td>
-                    <h5>LEGAL</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
-                    </ul>
-                </td>
-                <td>
-                    <h5>SOCIAL MEDIA</h5>
-                    <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
-                    </ul>
-                </td>
-            </tr>
-        </table>
+        <div class="container">
+            <table class="table table-bordered text-center">
+                <tr>
+                    <td>
+                        <a href="/" class="d-flex align-items-center link-dark text-decoration-none">
+                            <img class="img-fluid" src="{{ asset('assets/img/hun.png') }}" alt="" width="60rem">
+                        </a>
+                    </td>
+                    <td>
+                        <h5>NAVIGATION</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-muted">Home</a></li>
+                            <li class="nav-item mb-2"><a href="/about" class="nav-link p-0 text-muted">About</a></li>
+                            <li class="nav-item mb-2"><a href="/news" class="nav-link p-0 text-muted">News</a></li>
+                            <li class="nav-item mb-2"><a href="/media" class="nav-link p-0 text-muted">Media</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Works</a></li>
+                            <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Contact</a></li>
+                        </ul>
+                    </td>
+                    <td>
+                        <h5>LEGAL</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Terms Of Use</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy Policy</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
+                        </ul>
+                    </td>
+                    <td>
+                        <h5>SOCIAL MEDIA</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Facebook</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Youtube</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Linkedin</a></li>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        
     </footer>
 
     <p class="text-center pb-4">All rights reserved &copy; 2022 Hari Usahawan Negara</p>
