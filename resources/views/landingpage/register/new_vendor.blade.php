53de4908088c1444b008f9e22b1b14615dccba57
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-26 23:09:55 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -20,7 +20,7 @@ p {
 
 #heading {
     text-transform: uppercase;
-    color: #673AB7;
+    color: orange;
     font-weight: normal
 }
 
@@ -69,13 +69,13 @@ p {
     -moz-box-shadow: none !important;
     -webkit-box-shadow: none !important;
     box-shadow: none !important;
-    border: 1px solid #673AB7;
+    border: 1px solid orange;
     outline-width: 0
 }
 
 #msform .action-button {
     width: 100px;
-    background: #673AB7;
+    background: orange;
     font-weight: bold;
     color: white;
     border: 0 none;
@@ -117,14 +117,14 @@ p {
 
 .fs-title {
     font-size: 25px;
-    color: #673AB7;
+    color: orange;
     margin-bottom: 15px;
     font-weight: normal;
     text-align: left
 }
 
 .purple-text {
-    color: #673AB7;
+    color: orange;
     font-weight: normal
 }
 
@@ -148,7 +148,7 @@ p {
 }
 
 #progressbar .active {
-    color: #673AB7
+    color: orange
 }
 
 #progressbar li {
@@ -206,7 +206,7 @@ p {
 
 #progressbar li.active:before,
 #progressbar li.active:after {
-    background: #673AB7
+    background: orange
 }
 
 .progress {
@@ -214,7 +214,7 @@ p {
 }
 
 .progress-bar {
-    background-color: #673AB7
+    background-color: orange
 }
 
 .fit-image {
@@ -235,7 +235,7 @@ p {
                 <div class="row justify-content-center">
                     <div class="col-11 col-sm-9 col-md-7 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2"> --}}
                         {{-- <div class="card px-0 pt-4 pb-0 mt-3 mb-3"> --}}
-                            <form id="msform">
+                            <form id="msform" action="{{ url('new-registration/store') }}" method="POST" enctype="multipart/form-data">
                                 <!-- progressbar -->
                                 <ul id="progressbar">
                                     <li class="active" id="account"><strong>Personal</strong></li>
@@ -250,13 +250,21 @@ p {
                                     <div class="form-card">
                                         <div class="row">
                                             <div class="col-7">
-                                                <h2 class="fs-title">Account Information:</h2>
+                                                <h2 class="fs-title">Vendor Information:</h2>
                                             </div>
                                             <div class="col-5">
                                                 <h2 class="steps">Step 1 - 4</h2>
                                             </div>
-                                        </div> <label class="fieldlabels">Email: *</label> <input type="email" name="email" placeholder="Email Id" /> <label class="fieldlabels">Username: *</label> <input type="text" name="uname" placeholder="UserName" /> <label class="fieldlabels">Password: *</label> <input type="password" name="pwd" placeholder="Password" /> <label class="fieldlabels">Confirm Password: *</label> <input type="password" name="cpwd" placeholder="Confirm Password" />
-                                    </div> <input type="button" name="next" class="next action-button" value="Next" />
+                                        </div> 
+
+                                        <label class="fieldlabels">Email: *</label> 
+                                        <input type="email" name="email" placeholder="Email Id" /> 
+
+                                        <label class="fieldlabels">Username: *</label> 
+                                        <input type="text" name="uname" placeholder="UserName" /> <label class="fieldlabels">Password: *</label> <input type="password" name="pwd" placeholder="Password" /> <label class="fieldlabels">Confirm Password: *</label> <input type="password" name="cpwd" placeholder="Confirm Password" />
+                                    </div> 
+
+                                    <input type="button" name="next" class="next action-button" value="Next" />
                                 </fieldset>
                                 <fieldset>
                                     <div class="form-card">
@@ -267,8 +275,15 @@ p {
                                             <div class="col-5">
                                                 <h2 class="steps">Step 2 - 4</h2>
                                             </div>
-                                        </div> <label class="fieldlabels">First Name: *</label> <input type="text" name="fname" placeholder="First Name" /> <label class="fieldlabels">Last Name: *</label> <input type="text" name="lname" placeholder="Last Name" /> <label class="fieldlabels">Contact No.: *</label> <input type="text" name="phno" placeholder="Contact No." /> <label class="fieldlabels">Alternate Contact No.: *</label> <input type="text" name="phno_2" placeholder="Alternate Contact No." />
-                                    </div> <input type="button" name="next" class="next action-button" value="Next" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+                                        </div> 
+                                        
+                                        <label class="fieldlabels">First Name: *</label> 
+                                        <input type="text" name="fname" placeholder="First Name" /> 
+                                        
+                                        <label class="fieldlabels">Last Name: *</label> 
+                                        <input type="text" name="lname" placeholder="Last Name" /> <label class="fieldlabels">Contact No.: *</label> <input type="text" name="phno" placeholder="Contact No." /> <label class="fieldlabels">Alternate Contact No.: *</label> <input type="text" name="phno_2" placeholder="Alternate Contact No." />
+                                    </div>
+                                    <input type="button" name="next" class="next action-button" value="Next" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
                                 </fieldset>
                                 <fieldset>
                                     <div class="form-card">
@@ -279,8 +294,17 @@ p {
                                             <div class="col-5">
                                                 <h2 class="steps">Step 3 - 4</h2>
                                             </div>
-                                        </div> <label class="fieldlabels">Upload Your Photo:</label> <input type="file" name="pic" accept="image/*"> <label class="fieldlabels">Upload Signature Photo:</label> <input type="file" name="pic" accept="image/*">
-                                    </div> <input type="button" name="next" class="next action-button" value="Submit" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+                                        </div> 
+                                        <label class="fieldlabels">Upload Your Photo:</label> 
+                                        <input type="file" name="pic" accept="image/*"> 
+
+                                        <label class="fieldlabels">Upload Signature Photo:</label> 
+                                        <input type="file" name="pic" accept="image/*">
+                                    </div> 
+
+                                    <input type="button" name="next" class="next action-button" value="Submit" /> 
+                                    <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
+
                                 </fieldset>
                                 <fieldset>
                                     <div class="form-card">
@@ -291,11 +315,18 @@ p {
                                             <div class="col-5">
                                                 <h2 class="steps">Step 4 - 4</h2>
                                             </div>
-                                        </div> <br><br>
-                                        <h2 class="purple-text text-center"><strong>SUCCESS !</strong></h2> <br>
+                                        </div> 
+                                        <br><br>
+                                        <h2 class="purple-text text-center">
+                                            <strong>SUCCESS !</strong>
+                                        </h2> 
+                                        <br>
                                         <div class="row justify-content-center">
-                                            <div class="col-3"> <img src="https://i.imgur.com/GwStPmg.png" class="fit-image"> </div>
-                                        </div> <br><br>
+                                            <div class="col-3"> 
+                                                <img src="https://i.imgur.com/GwStPmg.png" class="fit-image"> 
+                                            </div>
+                                        </div> 
+                                        <br><br>
                                         <div class="row justify-content-center">
                                             <div class="col-7 text-center">
                                                 <h5 class="purple-text text-center">You Have Successfully Signed Up</h5>
