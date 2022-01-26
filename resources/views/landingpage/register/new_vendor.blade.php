2349f20c7b061990caf5e7efed754100f914e241
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-26 17:42:09 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -414,7 +414,7 @@
       // This function deals with validation of the form fields
       var x, y, i, valid = true;
       x = document.getElementsByClassName("tab");
-      y = x[currentTab].getElementsById("validate");
+      y = x[currentTab].getElementById("validate");
       // A loop that checks every input field in the current tab:
       for (i = 0; i < y.length; i++) {
         // If a field is empty...
