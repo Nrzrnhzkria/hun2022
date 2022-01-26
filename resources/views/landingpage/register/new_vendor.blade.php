f847b616a3aa7d28a37bb8065f5232a4b97d4b27
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-26 22:59:53 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -235,7 +235,7 @@ p {
             {{-- <div class="container-fluid">
                 <div class="row justify-content-center">
                     <div class="col-11 col-sm-9 col-md-7 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2"> --}}
-                        <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
+                        {{-- <div class="card px-0 pt-4 pb-0 mt-3 mb-3"> --}}
                             <h2 id="heading">Sign Up Your User Account</h2>
                             <p>Fill all form field to go to next step</p>
                             <form id="msform">
@@ -307,7 +307,7 @@ p {
                                     </div>
                                 </fieldset>
                             </form>
-                        </div>
+                        {{-- </div> --}}
                     {{-- </div>
                 </div>
             </div> --}}
