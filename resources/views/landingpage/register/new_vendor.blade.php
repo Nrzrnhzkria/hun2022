b60561ae0b668badbafa0716e3b5f42af88cf13e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-26 17:25:05 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -66,30 +66,31 @@
         <div class="card px-4 py-4">
             <form id="regForm" action="{{ url('new-registration/store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <h1>Register:</h1>
                 <!-- One "tab" for each step in the form: -->
-                <div class="tab">Name:
-                <p><input placeholder="First name..." oninput="this.className = ''" name="fname"></p>
-                <p><input placeholder="Last name..." oninput="this.className = ''" name="lname"></p>
+                <div class="tab">
+                    <div class="fw-bold px-2 py-2" style="background-color: orange">Exhibitor Information</div>
+                    Name:
+                    <p><input placeholder="First name..." class="form-control form-control-sm" oninput="this.className = ''" name="fname"></p>
+                    <p><input placeholder="Last name..." class="form-control form-control-sm" oninput="this.className = ''" name="lname"></p>
                 </div>
                 <div class="tab">Contact Info:
-                <p><input placeholder="E-mail..." oninput="this.className = ''" name="email"></p>
-                <p><input placeholder="Phone..." oninput="this.className = ''" name="phone"></p>
+                    <p><input placeholder="E-mail..." class="form-control form-control-sm" oninput="this.className = ''" name="email"></p>
+                    <p><input placeholder="Phone..." class="form-control form-control-sm" oninput="this.className = ''" name="phone"></p>
                 </div>
                 <div class="tab">Birthday:
-                <p><input placeholder="dd" oninput="this.className = ''" name="dd"></p>
-                <p><input placeholder="mm" oninput="this.className = ''" name="nn"></p>
-                <p><input placeholder="yyyy" oninput="this.className = ''" name="yyyy"></p>
+                    <p><input placeholder="dd" class="form-control form-control-sm" oninput="this.className = ''" name="dd"></p>
+                    <p><input placeholder="mm" class="form-control form-control-sm" oninput="this.className = ''" name="nn"></p>
+                    <p><input placeholder="yyyy" class="form-control form-control-sm" oninput="this.className = ''" name="yyyy"></p>
                 </div>
                 <div class="tab">Login Info:
-                <p><input placeholder="Username..." oninput="this.className = ''" name="uname"></p>
-                <p><input placeholder="Password..." oninput="this.className = ''" name="pword" type="password"></p>
+                    <p><input placeholder="Username..." class="form-control form-control-sm" oninput="this.className = ''" name="uname"></p>
+                    <p><input placeholder="Password..." class="form-control form-control-sm" oninput="this.className = ''" name="pword" type="password"></p>
                 </div>
                 <div style="overflow:auto;">
-                <div style="float:right;">
-                    <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-                    <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
-                </div>
+                    <div style="float:right;">
+                        <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+                        <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+                    </div>
                 </div>
                 <!-- Circles which indicates the steps of the form: -->
                 <div style="text-align:center;margin-top:40px;">
