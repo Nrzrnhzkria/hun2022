5e96fa6b59cba7220cb687b51c53c37a1fba592e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-27 11:47:00 +0800

    Update organization.blade.php

--- resources/views/landingpage/about/organization.blade.php
+++ resources/views/landingpage/about/organization.blade.php
@@ -7,8 +7,11 @@
 @section('content')
 
 <div class="container py-4">
-    <div class="card px-4 py-4">
-        <img class="img-fluid" src="{{ asset('assets/img/organization.png') }}" width="60rem">
+    <div class="row px-2">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mb-3 border-bottom">
+            <h1>Organizational Chart</h1>
+        </div>
+        <img class="img-fluid" src="{{ asset('assets/img/organization.png') }}" width="600rem">
     </div>
 </div>
 
