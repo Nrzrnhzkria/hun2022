d33f1626b395bf47764c467541012cb3df3dfb79
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-27 09:19:17 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -417,8 +417,8 @@ p {
                                         @endforeach
                                     </div> 
 
-                                    <button type="submit" class="btn btn-warning fw-bold">Next <i class="bi bi-chevron-double-right"></i></button>
-                                    {{-- <input type="button" name="submit" class="submit action-button" value="Submit" /> --}}
+                                    {{-- <button type="submit" class="btn btn-warning fw-bold">Next <i class="bi bi-chevron-double-right"></i></button> --}}
+                                    <input type="button" name="submit" class="submit action-button" value="Submit" />
                                     <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
 
                                 </fieldset>
@@ -706,9 +706,12 @@ p {
             .css("width",percent+"%")
         }
 
-        $(".submit").click(function(){
+        $('.submit').on("click",function() {
             return false;
-        })
+        });
+        // $(".submit").click(function(){
+        //     return false;
+        // })
 
     });
 </script>
