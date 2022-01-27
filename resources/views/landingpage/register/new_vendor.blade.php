8abff47b1c61d7894dd8f71b2235308de4aa8c1e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-27 15:42:24 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -257,7 +257,17 @@
 
     });
 
-    $('#total').text(sum)
+    // Create our number formatter.
+    var formatter = new Intl.NumberFormat('ms-MY', {
+        style: 'currency',
+        currency: 'MYR',
+
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
+    $('#total').formatter.format(sum);
 
 }).trigger("change")
 </script>
