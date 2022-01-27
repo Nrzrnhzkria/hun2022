fe66b13c580504727f7460d51863f165b2bdb434
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-27 13:01:59 +0800

    update

--- resources/views/landingpage/about/organization.blade.php
+++ resources/views/landingpage/about/organization.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-<div class="container py-4">
+<div class="container">
     <div class="row px-2">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-5 pb-2 mb-3 border-bottom">
             <h1>Organizational Chart</h1>
