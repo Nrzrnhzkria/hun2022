ef790a6fedce096af262d11bf744d262e15fcf05
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-27 13:00:28 +0800

    Update objective.blade.php

--- resources/views/landingpage/about/objective.blade.php
+++ resources/views/landingpage/about/objective.blade.php
@@ -13,17 +13,17 @@
         </div>
         
         <div class="col-md-12">
-            <div class="card" style="background-color: orange">
+            <div class="card shadow border-0" style="background-color: orange">
                 <div class="card-body p-4">
 
                     <ol>
-                        <li>National Entrepreneur Day HUN 2022 for the first time conducted/commencing/premiering in Malaysia and will be annual celebration for entrepreneurs</li>
-                        <li>Introducing local entrepreneur brands to all Malaysians</li>
-                        <li>Nominate Special Award to productive entrepreneurs that deserve recognition for their success</li>
-                        <li>Entrepreneurial knowledge sharing about business from entrepreneurs to visitors</li>
-                        <li>Establish a wide network amongst entrepreneurs in upholding the economic globalization</li>
-                        <li>Set up as milestone for Malaysians citizen to cultivate interest in develop into an entrepreneur</li>
-                        <li>Gather entrepreneurship in Malaysia from  government ministry government agencies, corporate, private NGOs and individuals</li>
+                        <li class="pb-2">National Entrepreneur Day HUN 2022 for the first time conducted/commencing/premiering in Malaysia and will be annual celebration for entrepreneurs</li>
+                        <li class="pb-2">Introducing local entrepreneur brands to all Malaysians</li>
+                        <li class="pb-2">Nominate Special Award to productive entrepreneurs that deserve recognition for their success</li>
+                        <li class="pb-2">Entrepreneurial knowledge sharing about business from entrepreneurs to visitors</li>
+                        <li class="pb-2">Establish a wide network amongst entrepreneurs in upholding the economic globalization</li>
+                        <li class="pb-2">Set up as milestone for Malaysians citizen to cultivate interest in develop into an entrepreneur</li>
+                        <li class="pb-2">Gather entrepreneurship in Malaysia from  government ministry government agencies, corporate, private NGOs and individuals</li>
                     </ol>
                 </div>
             </div>
