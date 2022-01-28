f22d62132fc14b739ea5a06500ab06fdc3ebcc4f
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-28 12:28:41 +0800

    Update VendorController.php

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -47,8 +47,8 @@ class VendorController extends Controller
     {
         $vendor_ic = $get_ic;
 
-        $booth = Booth::orderBy('id', 'desc')->get();
-        $booth_details = BoothDetails::orderBy('id', 'desc')->get();
+        $booth = Booth::orderBy('id', 'asc')->get();
+        $booth_details = BoothDetails::orderBy('id', 'asc')->get();
 
         $count = 1;
 
