f736a8025e1ed54e96ca112f52862e6547c024d4
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-28 14:46:52 +0800

    Update update.blade.php

--- resources/views/admin/admins/update.blade.php
+++ resources/views/admin/admins/update.blade.php
@@ -19,7 +19,7 @@
                     <form class="row g-3 px-3" method="POST" action="{{ url('edit-admin') }}/{{ $user->id }}">
                         @csrf
 
-                        <div class="row mb-3">
+                        <div class="row my-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
