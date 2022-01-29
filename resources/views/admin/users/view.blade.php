b5048bc0323ee48ce35509285466003418cbd1fd
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-29 10:31:39 +0800

    Update view.blade.php

--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -39,26 +39,26 @@
 
             <a href="/members" class="text-dark text-decoration-none">
                 <div class="card" style="height: 125px">
-                    <div class="row">
+                    <div class="row p-4">
                         <div class="col-md-4">
                             <i class="bi bi-person-badge-fill" style="font-size: 48pt"></i>
                         </div>
                         <div class="col-md-8">
-                            <h6>Member</h6>
-                            <b class="display-6">{{ number_format($member) }}</b>
+                            <h6 class="pt-4">Member</h6>
+                            <b class="display-6 pb-3">{{ number_format($member) }}</b>
                         </div>
                     </div>
                 </div>
             </a>
             <a href="/non-members" class="text-dark text-decoration-none">
                 <div class="card" style="height: 125px">
-                    <div class="row">
+                    <div class="row p-4">
                         <div class="col-md-4">
                             <i class="bi bi-person-badge" style="font-size: 48pt"></i>
                         </div>
                         <div class="col-md-8">
-                            <h6>Non-Member</h6>
-                            <b class="display-6">{{ number_format($nonmember) }}</b>
+                            <h6 class="pt-4">Non-Member</h6>
+                            <b class="display-6 pb-3">{{ number_format($nonmember) }}</b>
                         </div>
                     </div>
                 </div>
