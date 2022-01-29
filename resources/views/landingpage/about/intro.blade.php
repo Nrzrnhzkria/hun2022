c249145c5be745f40342d9f00d8abfde292287a2
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-29 22:38:20 +0800

    Update intro.blade.php

--- resources/views/landingpage/about/intro.blade.php
+++ resources/views/landingpage/about/intro.blade.php
@@ -17,8 +17,8 @@
 
             <div class="text-center fw-bold px-2 py-2" style="background-color: orange">INTRODUCTION</div>
 
-            <div class="row p-4">
-                <p style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DEWAN PERNIAGAAN USAHAWAN KECIL MALAYSIA,
+            <div class="row fw-bold p-4">
+                <p style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;DEWAN PERNIAGAAN USAHAWAN KECIL MALAYSIA,
                     abbreviated  (DPUKM) was formed on Mac 2013 is a platform for entrepreneurs that handle by 100% Bumiputera 
                     in collaboration with Ministries, Departments, Agencies, Cooperatives and the Private Sector. 
                 
@@ -26,7 +26,7 @@
                     founder and President and fully assisted by Datin Sri Norliah Che Mohamad as the Secretary General and several 
                     Members of the Supreme Council (SC) in the management of the association. 
                 </p> 
-                <p style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;With the existing capabilities and experience, 
+                <p style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;With the existing capabilities and experience, 
                     DPUKM capable to be a main platform and intended for helping small and medium-sized industry entrepreneurs move 
                     in line with the relevant Ministries and Agencies to succeed in the field of business ventured and launching a business.
                 </p>
