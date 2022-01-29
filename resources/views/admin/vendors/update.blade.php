3b03e280fd3c1978ff23dc9ac5d774c575f74389
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-29 08:52:46 +0800

    Update update.blade.php

--- resources/views/admin/vendors/update.blade.php
+++ resources/views/admin/vendors/update.blade.php
@@ -14,7 +14,7 @@
 
     <div class="row justify-content-center py-3">
         <div class="col-md-10">
-            <div class="card">
+            <div class="card py-3">
                 <div class="card-body">
                     <form class="row g-3 px-3" method="POST" action="{{ url('edit-vendor') }}/{{ $vendor->id }}">
                         @csrf
