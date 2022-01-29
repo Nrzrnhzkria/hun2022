393ad22e2058f57f6dc2dcda14c9516e26849796
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-29 09:04:46 +0800

    Update update.blade.php

--- resources/views/admin/vendors/update.blade.php
+++ resources/views/admin/vendors/update.blade.php
@@ -19,6 +19,8 @@
                     <form class="row g-3 px-3" method="POST" action="{{ url('edit-vendor') }}/{{ $vendor->id }}">
                         @csrf
                         
+                        <h4 class="border-bottom">Personal Details</h4>
+                        
                         <div class="row p-3">
 
                             <div class="col-md-6 pb-2">
@@ -99,101 +101,6 @@
                         
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $vendor->name }}" required autocomplete="name">
-                
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $vendor->email }}" required autocomplete="email">
-                
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ $vendor->password }}" required autocomplete="new-password">
-            
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" value="{{ $vendor->password }}" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="phone_no" class="col-md-4 col-form-label text-md-end">Phone No</label>
-
-                            <div class="col-md-6">
-                                <input id="phone_no" type="text" class="form-control @error('phone_no') is-invalid @enderror" name="phone_no" value="{{ $vendor->phone_no }}" required autocomplete="phone_no">
-            
-                                @error('phone_no')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="ic_no" class="col-md-4 col-form-label text-md-end">IC No</label>
-
-                            <div class="col-md-6">
-                                <input id="ic_no" type="text" class="form-control @error('ic_no') is-invalid @enderror" name="ic_no" value="{{ $vendor->ic_no }}" required autocomplete="ic_no">
-
-                                @error('ic_no')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="role" class="col-md-4 col-form-label text-md-end">Role</label>
-
-                            <div class="col-md-6">
-                                <select class="form-select" aria-label="Default select example">
-                                    <option class="text-capitalize" value="{{ $vendor->role }}" selected>{{ $vendor->role }}</option>
-                                    <option value="superadmin">Superadmin</option>
-                                    <option value="admin">Admin</option>
-                                    <option value="advisor">Advisor</option>
-                                    <option value="members">Members</option>
-                                    <option value="vendor">Vendor</option>
-                                    <option value="user">User</option>
-                                </select>
-                            </div>
-                        </div>
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
