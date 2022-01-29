0249b4acdaa9ce78c5ed13345ee5211a3eae73e4
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-29 23:41:23 +0800

    Update vision_mission.blade.php

--- resources/views/landingpage/about/vision_mission.blade.php
+++ resources/views/landingpage/about/vision_mission.blade.php
@@ -8,9 +8,9 @@
 
 <div class="container">
     <div class="row px-2">
-        <h1 class="text-center border-bottom py-3">Vision & Mission</h1>
+        <h1 class="text-center border-bottom pt-3">Vision & Mission</h1>
         
-        <div class="col-md-12">
+        <div class="col-md-12 pt-3">
             <div class="card shadow border-0" style="background-color: orange">
                 <div class="card-body p-4">
 
