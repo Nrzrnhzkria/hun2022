31fb8f94ad4c06685cc90e746ada066650497cd9
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 00:43:45 +0800

    Update vision_mission.blade.php

--- resources/views/landingpage/about/vision_mission.blade.php
+++ resources/views/landingpage/about/vision_mission.blade.php
@@ -11,56 +11,56 @@
         <h1 class="text-center border-bottom pt-5">Vision & Mission</h1>
         
         <div class="col-md-12 pt-3">                
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
 
-                    1. DPUKM has a clearly outlined goal set out to elevate entrepreneurs especially among Bumiputera in Malaysia
+                1. DPUKM has a clearly outlined goal set out to elevate entrepreneurs especially among Bumiputera in Malaysia
+            
+            </div>   
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
                 
-                </div>   
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-                    
-                    Raise awareness and better understanding of various entrepreneurship opportunities publicly
-                    
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                2. Raise awareness and better understanding of various entrepreneurship opportunities publicly
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
 
-                    Provide guidance for Bumiputera novice entrepreneurs to venture into business sector
+                3. Provide guidance for Bumiputera novice entrepreneurs to venture into business sector
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
                     
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                4. Upgrading small and medium entrepreneurs (SMEs) transition in shaping more successful entrepreneurs
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                    
+                5. Creating awareness among the public in entrepreneurship
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                6. Integrate entrepreneurs with accessibility to expand networking
+                    
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                7. Realizing the dream of bumiputera entrepreneurs towards developing a more world leaders entrepreneurship
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                8. Recognize small and medium-sized enterprises entrepreneurs as the main economic catalyst country
+                
+            </div>                
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                9. Developing small and medium-sized entrepreneurs from Zero to Hero that contributing in economic along business sector
+                
+            </div>
+            <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                10. Offering opportunity of entrepreneurship append
                         
-                    Upgrading small and medium entrepreneurs (SMEs) transition in shaping more successful entrepreneurs
-                    
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-                        
-                    Creating awareness among the public in entrepreneurship
-                    
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-
-                    Integrate entrepreneurs with accessibility to expand networking
-                        
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-
-                    Realizing the dream of bumiputera entrepreneurs towards developing a more world leaders entrepreneurship
-                    
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-
-                    Recognize small and medium-sized enterprises entrepreneurs as the main economic catalyst country
-                    
-                </div>                
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-
-                    Developing small and medium-sized entrepreneurs from Zero to Hero that contributing in economic along business sector
-                    
-                </div>
-                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
-
-                    Offering opportunity of entrepreneurship append
-                            
-                </div>
+            </div>
         </div>
     </div>
 </div>
