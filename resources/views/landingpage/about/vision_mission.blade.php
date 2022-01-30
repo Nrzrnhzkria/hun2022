75fd7afdbc57cf9e203b542bc7cd113280647a3a
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 00:42:55 +0800

    Update vision_mission.blade.php

--- resources/views/landingpage/about/vision_mission.blade.php
+++ resources/views/landingpage/about/vision_mission.blade.php
@@ -10,24 +10,57 @@
     <div class="row px-2">
         <h1 class="text-center border-bottom pt-5">Vision & Mission</h1>
         
-        <div class="col-md-12 pt-3">
-            <div class="card shadow border-0" style="background-color: orange">
-                <div class="card-body p-4">
+        <div class="col-md-12 pt-3">                
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
 
-                    <ol class="fw-bold">
-                        <li class="pb-2">DPUKM has a clearly outlined goal set out to elevate entrepreneurs especially among Bumiputera in Malaysia</li>
-                        <li class="pb-2">Raise awareness and better understanding of various entrepreneurship opportunities publicly</li>
-                        <li class="pb-2">Provide guidance for Bumiputera novice entrepreneurs to venture into business sector</li>
-                        <li class="pb-2">Upgrading small and medium entrepreneurs (SMEs) transition in shaping more successful entrepreneurs</li>
-                        <li class="pb-2">Creating awareness among the public in entrepreneurship</li>
-                        <li class="pb-2">Integrate entrepreneurs with accessibility to expand networking</li>
-                        <li class="pb-2">Realizing the dream of bumiputera entrepreneurs towards developing a more world leaders entrepreneurship</li>
-                        <li class="pb-2">Recognize small and medium-sized enterprises entrepreneurs as the main economic catalyst country</li>
-                        <li class="pb-2">Developing small and medium-sized entrepreneurs from Zero to Hero that contributing in economic along business sector</li>
-                        <li class="pb-2">Offering opportunity of entrepreneurship append</li>
-                    </ol>
+                    1. DPUKM has a clearly outlined goal set out to elevate entrepreneurs especially among Bumiputera in Malaysia
+                
+                </div>   
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                    
+                    Raise awareness and better understanding of various entrepreneurship opportunities publicly
+                    
                 </div>
-            </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Provide guidance for Bumiputera novice entrepreneurs to venture into business sector
+                    
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                        
+                    Upgrading small and medium entrepreneurs (SMEs) transition in shaping more successful entrepreneurs
+                    
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+                        
+                    Creating awareness among the public in entrepreneurship
+                    
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Integrate entrepreneurs with accessibility to expand networking
+                        
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Realizing the dream of bumiputera entrepreneurs towards developing a more world leaders entrepreneurship
+                    
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Recognize small and medium-sized enterprises entrepreneurs as the main economic catalyst country
+                    
+                </div>                
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Developing small and medium-sized entrepreneurs from Zero to Hero that contributing in economic along business sector
+                    
+                </div>
+                <div class="card shadow border-0 p-4 mb-3" style="background-color: orange">
+
+                    Offering opportunity of entrepreneurship append
+                            
+                </div>
         </div>
     </div>
 </div>
