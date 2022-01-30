cea0bec5788206b879a78612f2eda14c8a55b9f7
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-30 23:40:41 +0800

    Update policy.blade.php

--- resources/views/landingpage/policy.blade.php
+++ resources/views/landingpage/policy.blade.php
@@ -1,61 +1,82 @@
-<p>PRIVACY POLICY</p>
-<p>At Hari Usahawan Negara, accessible from https://www.hariusahawannegara.com.my, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hari Usahawan Negara and how we use it.
-If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at help@hariusahawannegara.com.my</p>
+@extends('layouts.app')
 
- 
+@section('title')
+    Privacy & Policy
+@endsection
 
-<p>LOG FILES</p>
+@section('content')
 
-<p>Hari Usahawan Negara follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services’ analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users’ movement on the website, and gathering demographic information.</p>
+<div class="container">
+    <div class="row px-2">
+        <h1 class="text-center border-bottom pt-5">Privacy & Policy</h1>
 
- 
+        <div class="col-md-12 pt-3">
+            <div class="card p-4">
+                
+                <p>PRIVACY POLICY</p>
+                <p>At Hari Usahawan Negara, accessible from https://www.hariusahawannegara.com.my, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Hari Usahawan Negara and how we use it.
+                If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at help@hariusahawannegara.com.my</p>
 
-<p>COOKIES & WEB BEACONS</p>
+                
 
-<p>Like any other website, Hari Usahawan Negara uses ‘cookies’. These cookies are used to store information including visitors’ preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’ experience by customizing our web page content based on visitors’ browser type and/or other information.
+                <p>LOG FILES</p>
 
-For more general information on cookies, please read “What Are Cookies”.</p>
+                <p>Hari Usahawan Negara follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services’ analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users’ movement on the website, and gathering demographic information.</p>
 
- 
+                
 
-<p>GOOGLE DOUBLECLICK DART COOKIES</p>
+                <p>COOKIES & WEB BEACONS</p>
 
-<p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads</p>
+                <p>Like any other website, Hari Usahawan Negara uses ‘cookies’. These cookies are used to store information including visitors’ preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’ experience by customizing our web page content based on visitors’ browser type and/or other information.
 
- 
+                For more general information on cookies, please read “What Are Cookies”.</p>
 
-<p>PRIVACY POLICIES</p>
+                
 
-<p>You may consult this list to find the Privacy Policy for each of the advertising partners of Hari Usahawan Negara.
+                <p>GOOGLE DOUBLECLICK DART COOKIES</p>
 
-Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hari Usahawan Negara, which are sent directly to users’ browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+                <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads</p>
 
-Note that Hari Usahawan Negara has no access to or control over these cookies that are used by third-party advertisers.</p>
+                
 
- 
+                <p>PRIVACY POLICIES</p>
 
-<p>THIRD PARTY PRIVACY POLICIES</p>
+                <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Hari Usahawan Negara.
 
-<p>Hari Usahawan Negara‘s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
+                Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Hari Usahawan Negara, which are sent directly to users’ browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
 
-You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers’ respective websites. What Are Cookies?</p>
+                Note that Hari Usahawan Negara has no access to or control over these cookies that are used by third-party advertisers.</p>
 
- 
+                
 
-<p>CHILDREN’S INFORMATION</p>
+                <p>THIRD PARTY PRIVACY POLICIES</p>
 
-<p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+                <p>Hari Usahawan Negara‘s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
 
-Hari Usahawan Negara does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+                You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers’ respective websites. What Are Cookies?</p>
 
- 
+                
 
-<p>ONLINE PRIVACY POLICY ONLY</p>
+                <p>CHILDREN’S INFORMATION</p>
 
-<p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hari Usahawan Negara. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+                <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
 
- 
+                Hari Usahawan Negara does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
 
-<p>CONSENT</p>
+                
 
-<p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+                <p>ONLINE PRIVACY POLICY ONLY</p>
+
+                <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Hari Usahawan Negara. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+
+                
+
+                <p>CONSENT</p>
+
+                <p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
