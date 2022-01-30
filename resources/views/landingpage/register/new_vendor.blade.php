91a203f8e052568a6d90b83c1be8416be14e71d3
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-30 23:05:55 +0800

    Update new_vendor.blade.php

--- resources/views/landingpage/register/new_vendor.blade.php
+++ resources/views/landingpage/register/new_vendor.blade.php
@@ -16,7 +16,7 @@
     
             <div class="card px-4 py-4">
 
-                <div class="fw-bold px-2 py-2" style="background-color: orange">Exhibitor Information</div>
+                <div class="fw-bold px-2 py-2" style="background-color: orange">Exhibitor Information / <em style="font-size: 10pt;">Maklumat Pempamer</em></div>
     
                 @if ($errors->any())
                     <div class="alert alert-danger">
@@ -128,7 +128,7 @@
                     
                 </div>
 
-                <div class="fw-bold px-2 py-2" style="background-color: orange">Coupon Details (Optional)</div>
+                <div class="fw-bold px-2 py-2" style="background-color: orange">Coupon Details (Optional) / <em style="font-size: 10pt;">Maklumat Kupon</em></div>
                 
                 <div class="row p-3">                  
                     <div class="col-md-6 pb-2">
@@ -170,7 +170,7 @@
                     </div>
                 </div>
 
-                <div class="fw-bold px-2 py-2" style="background-color: orange">Booth Details</div>
+                <div class="fw-bold px-2 py-2" style="background-color: orange">Booth Details / <em style="font-size: 10pt;">Maklumat Gerai</em></div>
                 
                 <div class="row p-3">        
                     <div class="col-md-12 pb-2">
