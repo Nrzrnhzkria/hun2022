06c342e3c2502ca1530c96dacf2d60b984ad398f
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-30 23:20:33 +0800

    Update events.blade.php

--- resources/views/landingpage/events.blade.php
+++ resources/views/landingpage/events.blade.php
@@ -6,8 +6,22 @@
 
 @section('content')
 
-<div class="container py-4">
-    content
+<div class="container">
+    <div class="row px-2">
+        <h1 class="text-center border-bottom pt-5">Objective</h1>
+
+        <div class="col-md-12 pt-3">
+            <div class="card px-4 py-4">
+
+                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_1.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_2.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_3.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_4.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_5.png') }}">
+
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
