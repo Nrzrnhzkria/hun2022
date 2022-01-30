3f8e8ab2147d62feef7a73e707900f6e5003a6a8
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-30 21:32:56 +0800

    Update CouponController.php

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request; 
 use App\Models\Coupon;
+use App\Models\Membership;
+use App\Models\Vendor;
+use App\Models\VendorDetails;
 
 class CouponController extends Controller
 {
