37c66098cc9cb879c20739bd2a85cd9143affc55
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-30 09:09:04 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -77,30 +77,29 @@
       </div>
     </div>
 
-<script>
-    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
-    
-    new Chart("myChart", {
-      type: "line",
-      data: {
-        labels: xValues,
-        datasets: [{
-          fill: false,
-          lineTension: 0,
-          backgroundColor: "rgba(0,0,255,1.0)",
-          borderColor: "rgba(0,0,255,0.1)",
-          data: yValues
-        }]
-      },
-      options: {
-        legend: {display: false},
-        scales: {
-          yAxes: [{ticks: {min: 6, max:16}}],
+    <script>
+      var xValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+      var yValues = [55, 49, 44, 24, 15, 55, 49, 44, 24, 15, 44, 24, 15];
+      var barColors = ["red", "green","blue","orange","brown", "purple", "yellow", "cyan", "maroon","pink","magenta","cream", "violet"];
+      
+      new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+          }]
+        },
+        options: {
+          legend: {display: false},
+          title: {
+            display: true,
+            text: "World Wine Production 2018"
+          }
         }
-      }
-    });
-</script>
+      });
+      </script>
 
 {{-- Members chart --}}
 <script>
