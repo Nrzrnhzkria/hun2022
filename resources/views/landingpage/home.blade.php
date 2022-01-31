3e9dcaf2848ad115e35652e2c049e63d47244a16
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 17:40:00 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -114,7 +114,7 @@
                     <td>
                         <div class="col-md-3">
                             {{-- <a href="/news" class="text-dark text-decoration-none"> --}}
-                            <div class="card shadow" style="width: 23.5rem;">
+                            <div class="card shadow" style="width: 23rem;">
                                 <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                 <h5 class="card-title">{{ $new->title }}</h5>
