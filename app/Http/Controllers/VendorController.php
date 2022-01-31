2b79feb7b514f8056ce5e12df12760ae0898cabc
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 16:16:42 +0800

    Update VendorController.php

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -203,10 +203,10 @@ class VendorController extends Controller
         $amount = ($payment->amount)*100;
 
         $data = array(
-            // 'userSecretKey' => config('toyyibpay.key'),
-            // 'categoryCode' => config('toyyibpay.category'),
-            'userSecretKey' => 'a25txcs8-x59p-adcl-xwz7-1d3grr2p6c1p',
-            'categoryCode' => 'vokse6qd',
+            'userSecretKey' => config('toyyibpay.key'),
+            'categoryCode' => config('toyyibpay.category'),
+            // 'userSecretKey' => 'a25txcs8-x59p-adcl-xwz7-1d3grr2p6c1p',
+            // 'categoryCode' => 'vokse6qd',
             'billName' => 'HUN Vendor Registration',
             'billDescription' => $booth_name->booth_name . ' - ' . $booth_type->booth_type,
             'billPriceSetting' => 1,
@@ -225,13 +225,13 @@ class VendorController extends Controller
             'billChargeToCustomer' => 2
         );
 
-        $url = 'https://dev.toyyibpay.com/index.php/api/createBill';
+        $url = 'https://toyyibpay.com/index.php/api/createBill';
         $response = Http::asForm()->post($url, $data);
         $bill_code = $response->json()[0]['BillCode'];
 
         // dd($amount);
         // dd($response->json()); // to know error
-        return redirect('https://dev.toyyibpay.com/' . $bill_code); // return url
+        return redirect('https://toyyibpay.com/' . $bill_code); // return url
     }
 
     public function payment_status(Request $request){
