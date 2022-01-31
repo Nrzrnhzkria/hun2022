7e348061f32d73d4b2357251b23800e912aad392
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 17:24:27 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -114,10 +114,10 @@
                     <td>
                         <div class="col-md-3">
                             {{-- <a href="/news" class="text-dark text-decoration-none"> --}}
-                            <div class="card" style="width: 16.5rem; height: 20rem;">
+                            <div class="card" style="width: 16.5rem;">
                                 <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
                                 <div class="card-body">
-                                <h5 class="card-title">{{ $new->title }}</h5>
+                                <h5 class="card-title pb-4">{{ $new->title }}</h5>
                                 {{-- <p class="card-text">{{ $new->teaser}}</p> --}}
                                 <div class="text-end">
                                     <a href="{{ url('news') }}/{{ $new->id }}" class="btn btn-warning btn-sm fw-bold">See More >></a>
