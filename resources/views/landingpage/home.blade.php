1513fdd8bc9e91e927ee71c9146cdd9a680c548e
Nrzrnhzkria <nrz.work@gmail.com>
2022-01-31 17:30:01 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -112,20 +112,34 @@
                 <tr>
                 @foreach ($news->take(4) as $new)
                     <td>
-                        <div class="col-md-3">
-                            {{-- <a href="/news" class="text-dark text-decoration-none"> --}}
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+                            <div class="col">
+                              <div class="card h-100" style="width: 16.5rem;">
+                                <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                  <h5 class="card-title">{{ $new->title }}</h5>
+                                  {{-- <p class="card-text">{{ $new->teaser}}</p> --}}
+                                </div>
+                                <div class="card-footer text-end">
+                                    <a href="{{ url('news') }}/{{ $new->id }}" class="btn btn-warning btn-sm fw-bold">See More >></a>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                        {{-- <div class="col-md-3">
+                            {{-- <a href="/news" class="text-dark text-decoration-none"> 
                             <div class="card" style="width: 16.5rem;">
                                 <img src="{{ $new->img_name }}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $new->title }}</h5>
-                                    {{-- <p class="card-text">{{ $new->teaser}}</p> --}}
+                                    {{-- <p class="card-text">{{ $new->teaser}}</p> 
                                 </div>
                                 <div class="card-footer">
                                     <a href="{{ url('news') }}/{{ $new->id }}" class="btn btn-warning btn-sm fw-bold">See More >></a>
                                 </div>
                             </div>
-                            {{-- </a> --}}
-                        </div> 
+                             </a> 
+                        </div>  --}}
                     </td>        
                 @endforeach
                 </tr>
