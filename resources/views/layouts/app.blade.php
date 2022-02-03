bb77d41b2488f8ed0151f96b9f05d72fb57f8fa9
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-03 13:56:15 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KRV85XH');</script>
+    })(window,document,'script','dataLayer','GTM-T7BGM6T');</script>
     <!-- End Google Tag Manager -->
 
     <meta charset="utf-8">
@@ -21,18 +21,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/template.js') }}" defer></script>
-    <!-- Google Analytics -->
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
-    ga('create', 'UA-XXXX-Y', 'auto');
-    ga('send', 'pageview');
-    
-    </script>
-    <!-- End Google Analytics -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -55,7 +43,7 @@
 </head>
 <body class="m-0 p-0">
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRV85XH"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7BGM6T"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
