208bde3ead0f73d0dc75348c1d2388924437c7b7
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-03 22:23:49 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="There are 8 upcoming exhibition, booth, programs, mentoring and etc can help growth your business and acquire information about new industry developments. The exhibition aims to host more than 2000 booths from local and international products. The exhibitions which businesses and companies in a specific niche come together to showcase their brands and products or promote new business developments.">
+    <meta name="description" content="There are 8 upcoming exhibition, booth, programs, mentoring and etc can help growth your business and acquire information about new industry developments. The exhibition aims to host more than 2000 booths from local and international products. The exhibitions which businesses, companies or vendor in a specific niche come together to showcase their brands and products or promote new business developments.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
