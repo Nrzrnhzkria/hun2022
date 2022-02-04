09f046740ada6d4d2857db92f770864b170b374b
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-04 22:21:31 +0800

    Update home.blade.php

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -87,19 +87,15 @@
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
             </div>
             <div class="carousel-inner">
-                <div class="carousel-item">
+                <div class="carousel-item active">
                     <img src="{{ asset('assets/img/slider01.jpg') }}" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('assets/img/Screenshot_1.png') }}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
                     <img src="{{ asset('assets/img/TRUCK-1.png') }}" class="d-block w-100" alt="...">
                 </div>
-                <div class="carousel-item active">
+                <div class="carousel-item">
                     <img src="{{ asset('assets/img/bonanza.jpeg') }}" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
