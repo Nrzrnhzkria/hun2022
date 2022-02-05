0ca4b6585a30721115985408650e9e94991d0f75
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-05 23:53:12 +0800

    Update media.blade.php

--- resources/views/landingpage/media/media.blade.php
+++ resources/views/landingpage/media/media.blade.php
@@ -14,21 +14,23 @@
 }
 </style>
 
-<div class="container px-2">
-    <h1 class="text-center border-bottom pt-5">Media</h1>
-    
-    <div class="col-md-12 pt-3">
-    @foreach ($medias as $media)
-        <a href="{{ url('media') }}/{{ $media->id }}" class="text-dark text-decoration-none">  
+<div class="container">
+    <div class="row px-2">
+        <h1 class="text-center border-bottom pt-5">Media</h1>
+        
+        <div class="col-md-12 pt-3">
+        @foreach ($medias as $media)
             <div class="card shadow mb-3" style="width: 18rem;">
-                <img src="{{ $media->img_name }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $media->title }}</h5>
-                    <p class="card-text">{{ $media->teaser}}</p>
-                </div>
+                <a href="{{ url('media') }}/{{ $media->id }}" class="text-dark text-decoration-none">  
+                    <img src="{{ $media->img_name }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $media->title }}</h5>
+                        <p class="card-text">{{ $media->teaser}}</p>
+                    </div>
+                </a>
             </div>
-        </a>
-    @endforeach
+        @endforeach
+        </div>
     </div>
 </div>
 
