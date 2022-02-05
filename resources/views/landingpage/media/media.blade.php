37e361a416f25de8764f0962b4118ee8aa408ee3
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-05 23:51:47 +0800

    Update media.blade.php

--- resources/views/landingpage/media/media.blade.php
+++ resources/views/landingpage/media/media.blade.php
@@ -14,34 +14,21 @@
 }
 </style>
 
-<div class="container">
-    <div class="row px-2">
-        <h1 class="text-center border-bottom pt-5">Media</h1>
-        
-        @foreach ($medias as $media)
-            <a href="{{ url('media') }}/{{ $media->id }}" class="text-dark text-decoration-none">  
-                <div class="card shadow mb-3" style="width: 18rem;">
-                    <img src="{{ $media->img_name }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $media->title }}</h5>
-                        <p class="card-text">{{ $media->teaser}}</p>
-                    </div>
+<div class="container px-2">
+    <h1 class="text-center border-bottom pt-5">Media</h1>
+    
+    <div class="col-md-12 pt-3">
+    @foreach ($medias as $media)
+        <a href="{{ url('media') }}/{{ $media->id }}" class="text-dark text-decoration-none">  
+            <div class="card shadow mb-3" style="width: 18rem;">
+                <img src="{{ $media->img_name }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $media->title }}</h5>
+                    <p class="card-text">{{ $media->teaser}}</p>
                 </div>
-                {{-- <div class="card shadow mb-3">
-                    <div class="row g-0">
-                        <div class="col-md-1">
-                            <img src="{{ $media->img_name }}" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-11">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $media->title }}</h5>
-                                <p class="card-text">{{ $media->teaser}}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
-            </a>
-        @endforeach
+            </div>
+        </a>
+    @endforeach
     </div>
 </div>
 
