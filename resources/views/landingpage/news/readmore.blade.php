c4650b4e26ac6b6396eeb876c4d031d311b79d8d
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-06 00:01:48 +0800

    Update readmore.blade.php

--- resources/views/landingpage/news/readmore.blade.php
+++ resources/views/landingpage/news/readmore.blade.php
@@ -14,7 +14,7 @@
             <div class="card px-4 py-4">
                 <div class="row pb-4">
                     <div class="col-md-12 text-center">        
-                        <img class="img-fluid" src="{{ $news->img_name }}" alt="">
+                        <img class="img-fluid" src="{{ $news->img_name }}" style="width:50rem" alt="">
                     </div>
                 </div>
 
