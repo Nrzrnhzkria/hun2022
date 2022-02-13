bc7a02b7dc913e1fd2eddb7db6e0bcfbd8c12761
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-13 21:49:40 +0800

    Update pending_payment.blade.php

--- resources/views/landingpage/register/pending_payment.blade.php
+++ resources/views/landingpage/register/pending_payment.blade.php
@@ -9,13 +9,13 @@
     <div class="row p-5">
         <div class="text-center">
             <h3>Your payment is still pending!</h3>
-            <p>Click button below if you wish to proceed the payment</p>
             {{-- <h3>Bill ID : {{ $payment->senangpay_id }}</h3> --}}
             <div class="py-3" style="font-size: 10rem; color: red;">
                 <i class="bi bi-x-circle"></i>
             </div>
+            <p>Click button below if you wish to proceed the payment</p>
             <hr>
-            <a href="https://toyyibpay.com/{{ $payment->toyyib_billcode }}" class="btn btn-dark">Proceed >></a>
+            <a href="https://toyyibpay.com/{{ $payment->toyyib_billcode }}" class="btn btn-lg btn-warning">Proceed >></a>
         {{-- <p> Please feel free to contact us and show the <b>Bill ID</b> if you wish to proceed the payment, thank you.</p> --}}
         </div>
     </div>
