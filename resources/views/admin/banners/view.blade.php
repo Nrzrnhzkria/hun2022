b59340ad21cb191fe17ceb8d416e6726a7aa47b5
Nrzrnhzkria <nrz.work@gmail.com>
2022-02-14 14:04:56 +0800

    Update view.blade.php

--- resources/views/admin/banners/view.blade.php
+++ resources/views/admin/banners/view.blade.php
@@ -82,7 +82,7 @@
                                         <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                        <div class="modal-body">
+                                        <div class="modal-body text-start">
                                         Are you sure you want to delete this banner ?
                                         </div>
                                         <div class="modal-footer">
