0c77330d5fb3149b8e530c08e6c0be995323fc85
Nrzrnhzkria <nrz.work@gmail.com>
2022-03-02 22:40:00 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,18 +12,18 @@
                             <li class="nav-item mb-2"><p class="nav-link p-0 text-muted text-break">No 30-2, Jalan 9/23a, Off Jalan Usahawan, 53200 Setapak, Kuala Lumpur.</p></li>
                             <li class="nav-item mb-2"><p class="nav-link p-0 text-muted text-break">Tel:    
                                                                                                         <ul>
-                                                                                                            <li>03 - 4141 8311 (Office)</li>
-                                                                                                            <li>011 - 1061 2950 (DPUKM Admin HUN)</li>
-                                                                                                            <li>019 - 716 9628 (DPUKM DAH 2 Secretariat)</li>
-                                                                                                            <li>010 - 421 3816 (DPUKM Mohd Shahrin)</li>
+                                                                                                            <li class="text-muted text-break">Office : 03 - 4141 8311</li>
+                                                                                                            <li class="text-muted text-break">DPUKM Admin HUN : 011 - 1061 2950</li>
+                                                                                                            <li class="text-muted text-break">DPUKM DAH 2 Secretariat : 019 - 716 9628</li>
+                                                                                                            <li class="text-muted text-break">DPUKM Mohd Shahrin : 010 - 421 3816</li>
 
                                                                                                         </ul>
                                                                                                     </p>
                                                                                                 </li>
                             <li class="nav-item mb-2"><p class="nav-link p-0 text-muted text-break">Email: 
                                                                                                         <ul>
-                                                                                                            <li>Info.hun22@gmail.com</li>
-                                                                                                            <li>dahniaga@gmail.com</li>
+                                                                                                            <li class="text-muted text-break">Info.hun22@gmail.com</li>
+                                                                                                            <li class="text-muted text-break">dahniaga@gmail.com</li>
                                                                                                         </ul>
                                                                                                     </p>
                                                                                                 </li>
