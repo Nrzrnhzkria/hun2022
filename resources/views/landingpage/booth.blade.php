80c4ba56d6809cd6ba1fd5088f6831e040a8fc27
Nrzrnhzkria <nrz.work@gmail.com>
2022-03-18 20:33:13 +0800

    Update booth.blade.php

--- resources/views/landingpage/booth.blade.php
+++ resources/views/landingpage/booth.blade.php
@@ -10,26 +10,38 @@
     <div class="row px-2">
         <h1 class="text-center border-bottom pt-5">Booth</h1>
 
-        <div class="col-md-12 pt-3">
-            <div class="card mb-3">
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/1.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/2.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/3.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/4.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/5.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/6.png') }}">
             </div>
-            <div class="card mb-3">
+        </div>
+        <div class="col-md-6 pt-3">
+            <div class="card">
                 <img class="img-fluid" src="{{ asset('assets/img/layout/7.png') }}">
             </div>
         </div>
