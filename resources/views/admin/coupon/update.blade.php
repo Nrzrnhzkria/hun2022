8cd0f952a78603c9d2d3d1e250677cc76833ba40
Nrzrnhzkria <nrz.work@gmail.com>
2022-03-21 10:44:41 +0800

    Update update.blade.php

--- resources/views/admin/coupon/update.blade.php
+++ resources/views/admin/coupon/update.blade.php
@@ -28,12 +28,51 @@
                         @foreach ($coupons as $coupon)
                             @if ($vendor->id == $coupon->vendor_id)
                             <div class="col-md-4">
-                                <div class="card shadow mb-3">
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <button type="button" class="btn btn-primary">
+                                        <a href="{{ $coupon->img_name }}" class="text-dark text-decoration-none">
+                                            <div class="row g-0">
+                                              <div class="col-md-4">
+                                                <img src="{{ $coupon->img_name }}" class="img-fluid rounded-start" alt="...">
+                                              </div>
+                                              <div class="col-md-8">
+                                                <div class="card-body py-0">
+                                                    <p class="fw-bold pt-2">{{ $coupon->category }}</p>
+                                                </div>
+                                              </div>
+                                            </div>
+                                        </a>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $vendor->id }}"><i class="bi bi-trash"></i></button>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal{{ $vendor->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>Are you sure you want to delete this coupon ?</p>
+                                                    <p>This table may be affected after deletion :</p>
+                                                    <ul>
+                                                        <li>Coupon</li>
+                                                        <li>Redeem</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <a class="btn btn-danger" href="{{ url('delete-coupon') }}/{{ $coupon->id }}">Delete</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- <div class="card shadow mb-3">
+                                    <a href="{{ $coupon->img_name }}" class="text-dark text-decoration-none">
                                     <div class="row g-0">
                                       <div class="col-md-4">
-                                        <a href="{{ $coupon->img_name }}" class="text-dark text-decoration-none">
-                                            <img src="{{ $coupon->img_name }}" class="img-fluid rounded-start" alt="coupon">
-                                        </a>
+                                        <img src="{{ $coupon->img_name }}" class="img-fluid rounded-start" alt="...">
                                       </div>
                                       <div class="col-md-8">
                                         <div class="card-body py-0">
@@ -65,7 +104,8 @@
                                         </div>
                                       </div>
                                     </div>
-                                </div>
+                                    </a>
+                                </div> --}}
                             </div>
                             @endif
                         @endforeach
