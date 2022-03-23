6d1bdd92a5d5e9349dab31ee07eaa8fed43fd0e0
Nrzrnhzkria <nrz.work@gmail.com>
2022-03-23 23:24:06 +0800

    update

--- resources/views/landingpage/events.blade.php
+++ resources/views/landingpage/events.blade.php
@@ -12,19 +12,19 @@
 
         <div class="col-md-8 offset-md-2 pt-3">
             <div class="card mb-3">
-                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_1.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/1.png') }}">
             </div>
             <div class="card mb-3">
-                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_2.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/2.png') }}">
             </div>
             <div class="card mb-3">
-                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_3.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/3.png') }}">
             </div>
             <div class="card mb-3">
-                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_4.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/4.png') }}">
             </div>
             <div class="card">
-                <img class="img-fluid" src="{{ asset('assets/img/events/agenda_5.png') }}">
+                <img class="img-fluid" src="{{ asset('assets/img/events/5.png') }}">
             </div>
         </div>
     </div>
