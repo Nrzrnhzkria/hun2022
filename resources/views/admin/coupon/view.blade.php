0b90d355551197ebb99c1e1d63f62a537f5ce094
Nrzrnhzkria <nrz.work@gmail.com>
2022-03-25 14:41:46 +0800

    Update view.blade.php

--- resources/views/admin/coupon/view.blade.php
+++ resources/views/admin/coupon/view.blade.php
@@ -10,11 +10,14 @@
     
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
         <h1 class="h2">Coupon</h1>
+        @if(Auth::user()->role == 'Superadmin')
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
               <a href="/view-category" class="btn btn-outline-dark"><i class="bi bi-eye-fill"></i> View Category</a>
             </div>
         </div>
+        @else
+        @endif
     </div>
 
     @if ($message = Session::get('updatecoupon'))
